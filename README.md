0f627d9a66456666d0114e6541a7094a002d1b11
LIU Heyang <lhypds@gmail.com>
2023-05-17 17:59:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+
+Simple AI Chat
+==============
+
+Forked from https://github.com/openai/openai-quickstart-node  
+
+
+Dependencies
+------------
+
+OpenAI https://platform.openai.com/docs/introduction  
+React https://reactjs.org/  
+Next.js https://nextjs.org/  
+
+
+Setup
+-----
+
+1. Install the requirements
+
+   ```bash
+   $ npm install
+   ```
+
+2. Make a copy of the example environment variables file
+
+   ```bash
+   $ cp .env.example .env
+   ```
+
+3. Add [API key](https://platform.openai.com/account/api-keys) to the newly created `.env` file
+
+4. Run the app
+
+   ```bash
+   $ npm run dev
+   ```
---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [aiChatInput, setAiChatInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ aiChat: aiChatInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setAiChatInput("");
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Simple AI Chat</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h2>Simple AI Chat</h2>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="aiChat"
+            placeholder=""
+            value={aiChatInput}
+            onChange={(e) => setAiChatInput(e.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
