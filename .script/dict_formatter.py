6ebb4bc6ee10fc8ee39d4bd45dbb7c7d7ef9a6d2
LIU Heyang <lhypds@gmail.com>
2023-05-25 15:36:07 +0900

    Add dict formatter

---
+++ .script/dict_formatter.py
@@ -0,0 +1,12 @@
+import unicodedata
+
+input = open("dict.csv", 'r')
+output = open("dict_buff.csv", 'w')
+Lines = input.readlines()
+for line in Lines:
+    row_buff = line.split('\",\"')
+    if len(row_buff) != 2:
+        print(row_buff[0] + ", length is not vaild. length = " + str(len(row_buff)))
+        continue
+    row_buff[0] = unicodedata.normalize('NFKC', row_buff[0])  # full-width to half-width
+    output.write(','.join(row_buff))
