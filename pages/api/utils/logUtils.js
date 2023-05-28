c27272721f55ef0902943ba10dd15a63351b03bd
LIU Heyang <lhypds@gmail.com>
2023-05-28 22:08:45 +0900

    Log IP and browser

--- pages/api/utils/logUtils.js
+++ pages/api/utils/logUtils.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
 
-export function logfile(log) {
+export function logfile(log, req) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const browser = req.headers['user-agent'];
+  log = log + ",IP=" + ip + ",BSR=" + browser;
   fs.appendFile('./log.txt', log + '\n', function (err) {
     if (err) throw err;
   });
