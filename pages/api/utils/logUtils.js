6c41bd84154e587e44480fdc0b0be9bc345db641
LIU Heyang <lhypds@gmail.com>
2023-05-28 22:42:02 +0900

    Format history log

--- pages/api/utils/logUtils.js
+++ pages/api/utils/logUtils.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 
 export function logfile(log, req) {
-  const ip = req.headers['x-forwarded-for'];
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const browser = req.headers['user-agent'];
-  log = log + ",IP=" + ip + ",BSR=" + browser;
-  fs.appendFile('./log.txt', log + '\n', function (err) {
+  log = log + "\nIP=" + ip + "\nBSR=" + browser;
+  fs.appendFile('./log.txt', log + '\n\n', function (err) {
     if (err) throw err;
   });
 }
