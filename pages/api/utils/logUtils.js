4ab3a8eb4651906e95ee816c7b2eaed050581c21
LIU Heyang <lhypds@gmail.com>
2023-05-28 22:56:40 +0900

    One question log one row

--- pages/api/utils/logUtils.js
+++ pages/api/utils/logUtils.js
@@ -3,8 +3,8 @@ import fs from 'fs';
 export function logfile(log, req) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const browser = req.headers['user-agent'];
-  log = log + "\nIP=" + ip + "\nBSR=" + browser;
-  fs.appendFile('./log.txt', log + '\n\n', function (err) {
+  log = log.replaceAll("\n", "###RETURN###") + ",IP=" + ip + ",BSR=" + browser;
+  fs.appendFile('./log.txt', log + '\n', function (err) {
     if (err) throw err;
   });
 }
