bf685265a5ce627e0cd5506504f4ed6f5da0e430
LIU Heyang <lhypds@gmail.com>
2023-05-31 22:12:31 +0900

    Small change

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ export default function help(args) {
     ":ls - List all entries, same as :entry ls.\n" +
     ":search [keyword] - Search dictionary.\n" +
     ":log - Show current session log.\n" +
-    ":log [all|session_id] - Show all logs or all session logs.\n" +
+    ":log [all|session_id] - Show all logs or session logs.\n" +
     ":stats [show|hide|on|off] - Show/hide stats info.\n" +
     ":stream [on|off] - Switch on/off stream mode.\n" +
     ":clear - Clear output.\n";
