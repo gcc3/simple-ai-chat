caf02cddaff4b269af0365909cfecec47a1217a7
LIU Heyang <lhypds@gmail.com>
2023-05-31 22:01:49 +0900

    Not sure why but changed the token limit to 4000

--- pages/api/utils/promptUtils.js
+++ pages/api/utils/promptUtils.js
@@ -14,7 +14,7 @@ const fine_tune_prompt_end = process.env.FINE_TUNE_PROMPT_END ? process.env.FINE
 const prompt_prefix = process.env.PROMPT_PREFIX ? process.env.PROMPT_PREFIX : "";
 const prompt_suffix = process.env.PROMPT_SUFFIX ? process.env.PROMPT_SUFFIX : "";
 const max_tokens = process.env.MAX_TOKENS ? Number(process.env.MAX_TOKENS) : 700;
-const token_limit = 4097;
+const token_limit = 4000;
 const stream_console = process.env.STREAM_CONSOLE == "true" ? true : false;
 
 // Generate messages for chatCompletion
