8317131a839ad5ada4f178b32306b411043f96b6
LIU Heyang <lhypds@gmail.com>
2023-05-31 16:46:49 +0900

    Improve the log display

--- commands/log.js
+++ commands/log.js
@@ -15,7 +15,17 @@ export default async function log(args) {
     if (data.result.logs === "") {
       return "No log found.";
     } else {
-      return data.result.logs;
+
+      // Format the timestamp
+      const logs = data.result.logs.split("\n");
+      for (let i = 0; i < logs.length; i++) {
+        const log = logs[i];
+        const timestamp = log.substring(log.search("T=") + 2, log.search("T=") + 15);
+        const date = new Date(parseInt(timestamp));
+        logs[i] = date.toLocaleString() + " \n" + log.substring(16);
+      }
+
+      return logs;
     }
   } catch (error) {
     console.error(error);
