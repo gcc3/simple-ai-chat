2a98d438b34f38e0684b0d4f730d84fcdb9f7c47
LIU Heyang <lhypds@gmail.com>
2023-06-01 11:44:43 +0900

    Small change

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,6 @@
 export default function info(args) {
   const info = "Session ID: " + localStorage.getItem("queryId") + "\n" +
-               "Stats: " + localStorage.getItem("useStats") + "\n" +
+               "Show Stats: " + localStorage.getItem("useStats") + "\n" +
                "Stream: " + localStorage.getItem("useStream") + "\n";
 
   return info;
