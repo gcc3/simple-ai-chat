fdd83ef0a2dfdf2fa420d7c5d89f8553643727c2
LIU Heyang <lhypds@gmail.com>
2023-06-09 12:44:15 +0900

    Imprve role info

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@ export default function info(args) {
   const info = "Session ID: " + localStorage.getItem("queryId") + "\n" +
                "Show Stats: " + localStorage.getItem("useStats") + "\n" +
                "Stream: " + localStorage.getItem("useStream") + "\n" +
-               "Role: " + localStorage.getItem("role") + "\n";
+               "Role: " + (localStorage.getItem("role") || "default") + "\n";
 
   return info;
 }
