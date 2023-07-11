937d406d290a254340c7d7826627170916fc271a
LIU Heyang <lhypds@gmail.com>
2023-07-11 21:26:11 +0900

    Change default speak setting

--- pages/api/utils/speakUtils.js
+++ pages/api/utils/speakUtils.js
@@ -2,8 +2,8 @@
 export function speak(text) {
   var utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
-  utterance.volume = 1;  // 0 to 1
-  utterance.rate = 1;    // 0.1 to 10
+  utterance.volume = 0.7;  // 0 to 1
+  utterance.rate = 1.2;    // 0.1 to 10
   utterance.pitch = 2;   // 0 to 2
   utterance.lang = localStorage.getItem("lang");
   window.speechSynthesis.speak(utterance);
