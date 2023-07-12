214c0b28118a21756465701985770bbcf1435697
LIU Heyang <lhypds@gmail.com>
2023-07-12 23:16:37 +0900

    Fix the speak speed too fast

--- pages/api/utils/speakUtils.js
+++ pages/api/utils/speakUtils.js
@@ -3,7 +3,7 @@ export function speak(text) {
   var utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
   utterance.volume = 0.7;  // 0 to 1
-  utterance.rate = 1.2;    // 0.1 to 10
+  utterance.rate = 1;    // 0.1 to 10
   utterance.pitch = 2;     // 0 to 2
   utterance.lang = localStorage.getItem("lang");
   window.speechSynthesis.speak(utterance);
