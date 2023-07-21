7db18fb97305b3cd2e9fdda28adba2434a1fdbd4
LIU Heyang <lhypds@gmail.com>
2023-07-22 06:18:18 +0900

    Add voice info

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,8 @@ export default function info(args) {
                "Show Stats: " + localStorage.getItem("useStats") + "\n" +
                "Stream: " + localStorage.getItem("useStream") + "\n" +
                "Speak: " + localStorage.getItem("useSpeak") + "\n" +
-               "Language: " + (localStorage.getItem("lang") || "en") + "\n" +
+               "Voice: " + (localStorage.getItem("voice") || "default") + "\n" +
+               "Language: " + (localStorage.getItem("lang") || "en-US") + "\n" +
                "Location Service: " + localStorage.getItem("useLocation") + "\n" +
                "Location: (" + localStorage.getItem("location") + ")\n" +
                "Role: " + (localStorage.getItem("role") || "default");
