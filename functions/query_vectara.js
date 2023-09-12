59a0a351fc2cc5b75d466d00b3e15f836f981d80
LIU Heyang <lhypds@gmail.com>
2023-09-12 20:44:46 +0900

    Config the query result

--- functions/query_vectara.js
+++ functions/query_vectara.js
@@ -4,24 +4,36 @@ export default async function queryVectara(query) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        'Accept': 'application/json', 
         "customer-id": process.env.VECTARA_CUSTOMER_ID,
         "x-api-key": process.env.VECTARA_API_KEY,
       },
       body: JSON.stringify({
-        "query": query,
-        "corpusKey": [
+        "query": [
           {
-            "customerId": process.env.VECTARA_CUSTOMER_ID,
-            "corpusId": process.env.VECTARA_CORPUS_ID,
-            "semantics": "DEFAULT",
-            "lexicalInterpolationConfig": {
-              "lambda": 0
-            }
+            "query": query,
+            "start": 0,
+            "numResults": 1,
+            "contextConfig": {
+              "charsBefore": 30,
+              "charsAfter": 30,
+              "sentencesBefore": 3,
+              "sentencesAfter": 3,
+              "startTag": "<b>",
+              "endTag": "</b>"
+            },
+            "corpusKey": [
+              {
+                "customerId": process.env.VECTARA_CUSTOMER_ID,
+                "corpusId": process.env.VECTARA_CORPUS_ID,
+                "semantics": "DEFAULT",
+              }
+            ],
           }
         ]
       }),
   });
 
   const data = await response.json();
-  return data.result;
+  return data.responseSet[0].response[0].text;
 }
