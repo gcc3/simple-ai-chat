6056b0f98cf3a2772477d3b549224d6990f32108
LIU Heyang <lhypds@gmail.com>
2023-09-27 02:23:12 +0900

    Fix dark theme color

--- utils/themeUtils.js
+++ utils/themeUtils.js
@@ -2,13 +2,13 @@
 export function setTheme(theme) {
   if (theme == "light") {
     document.documentElement.style.setProperty('--background-color', 'transparent');
-    document.documentElement.style.setProperty('--input-box-color', 'transparent');
-    document.documentElement.style.setProperty('--output-text-color', '#000000');
+    document.documentElement.style.setProperty('--text-color', '#000000');
+    document.documentElement.style.setProperty('--button-color', '#d3d3d3');
   }
   
   if (theme == "dark") {
     document.documentElement.style.setProperty('--background-color', '#000000');
-    document.documentElement.style.setProperty('--input-box-color', '#000000');
-    document.documentElement.style.setProperty('--output-text-color', '#ffffff');
+    document.documentElement.style.setProperty('--text-color', '#ffffff');
+    document.documentElement.style.setProperty('--button-color', '#2b2b2b');
   }
 }
