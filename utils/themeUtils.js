6327a3cbf162a9c457490725d5596926d999d4d6
LIU Heyang <lhypds@gmail.com>
2023-09-27 02:41:59 +0900

    Fix dark mode color

--- utils/themeUtils.js
+++ utils/themeUtils.js
@@ -3,12 +3,20 @@ export function setTheme(theme) {
   if (theme == "light") {
     document.documentElement.style.setProperty('--background-color', 'transparent');
     document.documentElement.style.setProperty('--text-color', '#000000');
+    document.documentElement.style.setProperty('--placeholder-color', '#8e8ea0');
+    document.documentElement.style.setProperty('--border-color', '#ccc');
     document.documentElement.style.setProperty('--button-color', '#d3d3d3');
+    document.documentElement.style.setProperty('--button-hover-color', '#828282');
+    document.documentElement.style.setProperty('--button-text-color', '#fff');
   }
   
   if (theme == "dark") {
     document.documentElement.style.setProperty('--background-color', '#000000');
-    document.documentElement.style.setProperty('--text-color', '#ffffff');
+    document.documentElement.style.setProperty('--text-color', '#00f700');
+    document.documentElement.style.setProperty('--placeholder-color', '#027a02');
+    document.documentElement.style.setProperty('--border-color', '#333333');
     document.documentElement.style.setProperty('--button-color', '#2b2b2b');
+    document.documentElement.style.setProperty('--button-hover-color', '#3d3d3d');
+    document.documentElement.style.setProperty('--button-text-color', '#7f7f7f');
   }
 }
