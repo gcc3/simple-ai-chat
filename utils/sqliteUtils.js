38b58141de345e6fc6af5b8262c21f282e5867e4
LIU Heyang <lhypds@gmail.com>
2023-10-03 20:57:08 +0900

    Create users table

--- utils/sqliteUtils.js
+++ utils/sqliteUtils.js
@@ -11,6 +11,45 @@ const createDatabaseFile = () => {
   });
 };
 
+// Initialize the database
+const initializeDatabase = (db) => {
+  return new Promise((resolve, reject) => {
+    
+    // Create logs table
+    const createLogsTable = `
+      CREATE TABLE IF NOT EXISTS logs (
+          id INTEGER PRIMARY KEY,
+          time INTEGER NOT NULL,
+          session INTEGER NOT NULL,
+          log TEXT NOT NULL
+      );`;
+      
+    db.run(createLogsTable, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      
+      // Create users table
+      const createUsersTable = `
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            pass TEXT NOT NULL,
+            settings TEXT,
+            last_login TEXT
+        );`;
+        
+      db.run(createUsersTable, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        
+        resolve();
+      });
+    });
+  });
+};
+
 const getDatabaseConnection = async () => {
   if (!fs.existsSync('./db.sqlite')) {
     console.log("Database not exist, trying to create.")
@@ -27,25 +66,7 @@ const getDatabaseConnection = async () => {
   });
 };
 
-// Initialize the database
-const initializeDatabase = (db) => {
-  return new Promise((resolve, reject) => {
-    const createLogsTable = `
-    CREATE TABLE IF NOT EXISTS logs (
-        id INTEGER PRIMARY KEY,
-        time INTEGER NOT NULL,
-        session INTEGER NOT NULL,
-        log TEXT NOT NULL
-    );`;
-    db.run(createLogsTable, (err) => {
-      if (err) {
-        reject(err);
-      }
-      resolve();
-    });
-  });
-};
-
+// I. logs
 // Get logs by session
 const getLogs = async (session) => {
   const db = await getDatabaseConnection();
@@ -82,7 +103,122 @@ const insertLog = async (time, session, log) => {
   }
 };
 
+// II. users
+const getUsers = async (name) => {
+  const db = await getDatabaseConnection();
+  try {
+    return await new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM users WHERE name = ?`, [name], (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  } finally {
+    db.close();
+  }
+};
+
+const insertUser = async (name, pass, settings, last_login) => {
+  const db = await getDatabaseConnection();
+  try {
+    return await new Promise((resolve, reject) => {
+      const stmt = db.prepare("INSERT INTO users (name, pass, settings, last_login) VALUES (?, ?, ?, ?)");
+      stmt.run([name, pass, settings, last_login], function (err) {
+        if (err) {
+          reject(err);
+        }
+        // This `this.lastID` provides the ID of the last inserted row.
+        resolve(this.lastID);
+      });
+      stmt.finalize();
+    });
+  } finally {
+    db.close();
+  }
+};
+
+const deleteUser = async (userId) => {
+  const db = await getDatabaseConnection();
+  try {
+    return await new Promise((resolve, reject) => {
+      const stmt = db.prepare("DELETE FROM users WHERE id = ?");
+      stmt.run([userId], function (err) {
+        if (err) {
+          reject(err);
+        }
+        // This `this.changes` provides the number of rows deleted.
+        if (this.changes > 0) {
+          resolve(true); // Indicates successful deletion
+        } else {
+          resolve(false); // No rows were deleted (maybe user not found)
+        }
+      });
+      stmt.finalize();
+    });
+  } finally {
+    db.close();
+  }
+};
+
+const updateUserPass = async (userId, newPass) => {
+  const db = await getDatabaseConnection();
+  try {
+    return await new Promise((resolve, reject) => {
+      const stmt = db.prepare("UPDATE users SET pass = ? WHERE id = ?");
+      stmt.run([newPass, userId], function (err) {
+        if (err) {
+          reject(err);
+        }
+        // This `this.changes` provides the number of rows updated.
+        if (this.changes > 0) {
+          resolve(true); // Indicates successful update
+        } else {
+          resolve(false); // No rows were updated (maybe user not found)
+        }
+      });
+      stmt.finalize();
+    });
+  } finally {
+    db.close();
+  }
+};
+
+const updateUserLastLogin = async (userId, lastLogin) => {
+  const db = await getDatabaseConnection();
+  try {
+    return await new Promise((resolve, reject) => {
+      const stmt = db.prepare("UPDATE users SET last_login = ? WHERE id = ?");
+      stmt.run([lastLogin, userId], function (err) {
+        if (err) {
+          reject(err);
+        }
+        // This `this.changes` provides the number of rows updated.
+        if (this.changes > 0) {
+          resolve(true); // Indicates successful update
+        } else {
+          resolve(false); // No rows were updated (maybe user not found)
+        }
+      });
+      stmt.finalize();
+    });
+  } finally {
+    db.close();
+  }
+};
+
 module.exports = {
+
+  // logs
   getLogs,
-  insertLog
+  insertLog,
+
+  // users
+  getUsers,
+  insertUser,
+  deleteUser,
+  updateUserPass,
+  updateUserLastLogin
+
 };
