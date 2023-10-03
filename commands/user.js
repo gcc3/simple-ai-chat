575c12ea19e2022aadd61d1381079071d2e1bcfb
LIU Heyang <lhypds@gmail.com>
2023-10-03 22:07:15 +0900

    Add set pass and login

--- commands/user.js
+++ commands/user.js
@@ -1,6 +1,15 @@
 export default async function entry(args) {
   const command = args[0];
 
+  if (command === "login") {
+    if (args.length != 2) {
+      return "Usage: :user login [username]";
+    }
+
+    localStorage.setItem("user", args[1]);
+    return "Login successful."
+  }
+
   if (command === "add") {
     if (args.length != 2) {
       return "Usage: :user add [username]";
@@ -29,6 +38,36 @@ export default async function entry(args) {
     }
   }
 
+  if (command === "set" && args[1] === "pass") {
+    if (args.length != 3) {
+      return "Usage: :user set pass [password]";
+    }
+
+    try {
+      const response = await fetch('/api/user/update/pass', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: localStorage.getItem("user"),
+          password: args[2],
+        }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      return "Password updated."
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
   return "Usage: :user add [username]" + "\n" +
-         "       :user set pass [password]" + "\n";
+         "       :user set pass [password]" + "\n" +
+         "       :user login [username]";
 }
---
+++ pages/api/user/update/pass.js
@@ -0,0 +1,27 @@
+import { updateUserPass } from 'utils/sqliteUtils.js';
+
+export default async function (req, res) {
+  // Check if the method is POST.
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+  
+  const { user, password } = req.body;
+
+  // Basic validation
+  if (!user || !password) {
+    return res.status(400).json({ error: 'username and pass are required.' });
+  }
+
+  try {
+    const wasSuccessful = await updateUserPass(user, password);
+    if (wasSuccessful) {
+      return res.status(200).json({ success: true, message: "Password updated successfully" });
+    } else {
+      return res.status(400).json({ error: 'Failed to update password or user not found.' });
+    }
+  } catch (error) {
+    console.error('Error updating user password:', error);
+    return res.status(500).json({ error: 'An error occurred while updating the password.' });
+  }
+}
