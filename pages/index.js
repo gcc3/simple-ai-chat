487f2a580e612d959415842b7c4ed74b6af34796
LIU Heyang <lhypds@gmail.com>
2023-10-08 19:42:48 +0900

    Extract markdown formatter

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { speak, trySpeak } from "utils/speakUtils.js";
 import { setTheme } from "utils/themeUtils.js";
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleFullscreen, reverseFullscreen } from '../states/fullscreenSlice';
+import { markdownFormatter } from "utils/markdownUtils";
 
 // Status control
 const STATES = { IDLE: 0, DOING: 1 };
@@ -125,34 +126,10 @@ export default function Home() {
     const outputMutationObserver = new MutationObserver(mutationsList => {
       for (let mutation of mutationsList) {
         if (mutation.type === 'childList' || mutation.type === 'characterData') {
-          formatter(outputMutationObserver);
+          markdownFormatter(outputMutationObserver);
         }
       }
     });
-
-    const formatter = (observer) => {
-      // Temproary stop observing
-      observer.disconnect();
-
-      // Format the output
-      const outputElement = document.getElementById("output");
-      if (outputElement) {
-        const output = outputElement.innerHTML;
-
-        outputElement.innerHTML = output
-          .replace(/```([^`]+)```/g, '<pre>$1</pre>')                    // Replace the ```text``` with <pre> and </pre>
-          .replace(/(?<!`)`([^`]+)`(?!`)/g, '<code>$1</code>')           // Replace the `text` with <code> and </code>, but ignore ```text```
-          .replace(/<pre>\s*(\w+)?\s*<br>/g, '<pre>')                    // Replace <pre> language_name <br> with <pre><br>
-          .replace(/<\/pre><br><br>/g, '</pre><br>')                     // Replace <pre><br><br> with <pre><br>
-          .replace(/<br><\/pre>/g, '</pre>')                             // Replace <br></pre> with </pre>
-          .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')            // Replace the **text** with <strong> and </strong>
-          .replace(/\*([^*]+)\*/g, '<em>$1</em>')                        // Replace the *text* with <em> and </em>
-      }
-
-      // Resume observing
-      observer.observe(document.getElementById("output"), 
-      { childList: true, attributes: false, subtree: true, characterData: true });
-    }
 
     // Start observing
     outputMutationObserver.observe(document.getElementById("output"), 
---
+++ utils/markdownUtils.js
@@ -0,0 +1,27 @@
+export function markdownFormatter(observer) {
+  const outputElement = document.getElementById("output");
+  if (outputElement) {
+    
+    // Temproary stop observing
+    observer.disconnect();
+
+    // Format the output
+    const output = outputElement.innerHTML;
+    outputElement.innerHTML = output
+      .replace(/```([^`]+)```/g, '<pre>$1</pre>')                    // Replace the ```text``` with <pre> and </pre>
+      .replace(/(?<!`)`([^`]+)`(?!`)/g, '<code>$1</code>')           // Replace the `text` with <code> and </code>, but ignore ```text```
+      .replace(/<pre>\s*(\w+)?\s*<br>/g, '<pre>')                    // Replace <pre> language_name <br> with <pre><br>
+      .replace(/<\/pre><br><br>/g, '</pre><br>')                     // Replace <pre><br><br> with <pre><br>
+      .replace(/<br><\/pre>/g, '</pre>')                             // Replace <br></pre> with </pre>
+      .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')            // Replace the **text** with <strong> and </strong>
+      .replace(/\*([^*]+)\*/g, '<em>$1</em>')                        // Replace the *text* with <em> and </em>
+
+    // Resume observing
+    observer.observe(outputElement, { 
+      childList: true, 
+      attributes: false, 
+      subtree: true, 
+      characterData: true
+    });
+  }
+}
