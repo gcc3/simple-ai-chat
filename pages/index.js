459d15b00e4f4fa6bf0e8fe59fd33aac8020a2a8
LIU Heyang <lhypds@gmail.com>
2023-10-08 14:16:20 +0900

    Fix strong text weight

--- pages/index.js
+++ pages/index.js
@@ -145,6 +145,7 @@ export default function Home() {
           .replace(/```([^`]+)```/g, '<pre>$1</pre>')           // Replace the ```text``` with <pre> and </pre>
           .replace(/<pre>\s*(\w+)?\s*<br>/g, '<pre>')           // Replace <pre> language_name <br> with <pre><br>
           .replace(/<\/pre><br><br>/g, '</pre><br>')            // Replace <pre><br><br> with <pre><br>
+          .replace(/<br><\/pre>/g, '</pre>')                    // Replace <br></pre> with </pre>
           .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')   // Replace the **text** with <strong> and </strong>
           .replace(/\*([^*]+)\*/g, '<em>$1</em>');              // Replace the *text* with <em> and </em>
       }
