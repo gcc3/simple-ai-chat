79f9ea80dfdc19e80f7c72cd1470793b5d486dfd
LIU Heyang <lhypds@gmail.com>
2023-11-01 03:20:15 +0900

    Update policy and style

--- components/Copyrights.js
+++ components/Copyrights.js
@@ -2,7 +2,7 @@ import Markdown from 'react-markdown';
 
 function Copyrights() {
   return (
-    <div className="Copyrights text-sm mt-2 text-right">
+    <div className="Copyrights text-sm">
       © 2023 SIMPLE-AI.IO
     </div>
   );
--- components/UserDataPrivacy.js
+++ components/UserDataPrivacy.js
@@ -3,34 +3,34 @@ import remarkGfm from 'remark-gfm'
 
 function UserDataPrivacy() {
   const markdown = `
-  **1. Introduction**
+  **Introduction**
   
   Welcome to [SimpleAI](simple-ai.io). We are committed to protecting your personal information and your right to privacy.
   
-  **2. What Information We Collect**
+  **What Information We Collect**
   
   We collect minimal user information strictly necessary to provide and improve our services. This information may include:
   
   - Basic account information: Email address for account creation.
   - Usage data: Email address will be used for account registeration, password recovery and service updates notification.
   
-  **3. How We Use Your Information**
+  **How We Use Your Information**
   
   We use your information to provide, improve, and customize our services. We do not sell, rent, or trade your information to any third parties. 
   
-  **4. How We Store and Protect Your Information**
+  **How We Store and Protect Your Information**
   
   Your data is securely stored in our website's database. We implement a variety of security measures to maintain the safety of your personal information.
   
-  **5. Your Rights**
+  **Your Rights**
   
   You have the right to access, update, or delete your personal information at any time. When you delele your account, we will delete all your personal information from our database.
   
-  **6. Changes to This Policy**
+  **Changes to This Policy**
   
   We may modify this privacy policy at any time, so please review it frequently. Changes will take effect immediately upon their posting on the website.
   
-  **7. Contact Us**
+  **Contact Us**
   
   If you have any questions or concerns about this privacy policy, please contact us at soft@gochaichai.com.
   `
