4197b9afa2da3dbd8c95b11157f1a52a7002c4b2
LIU Heyang <lhypds@gmail.com>
2023-11-01 02:55:16 +0900

    Small change

--- components/Copyrights.js
+++ components/Copyrights.js
@@ -3,7 +3,7 @@ import Markdown from 'react-markdown';
 function Copyrights() {
   return (
     <div className="Copyrights text-sm mt-2 text-right">
-© 2023 SIMPLE-AI.IO
+      © 2023 SIMPLE-AI.IO
     </div>
   );
 }
