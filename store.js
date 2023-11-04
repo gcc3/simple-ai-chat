712498bbd5541ec832ee45c1e910483b44c18bcd
LIU Heyang <lhypds@gmail.com>
2023-11-04 13:59:31 +0900

    Small change

--- store.js
+++ store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import fullscreenReducer from './states/fullscreenSlice';
 
 // Redux store
+// redux-thunk is already set up by default with Redux Toolkit
 const store = configureStore({
   reducer: {
     isFullscreen: fullscreenReducer
