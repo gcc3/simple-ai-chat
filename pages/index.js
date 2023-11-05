3a9931a4130d13bcdb42c3e3fe85d2f80b529ae5
LIU Heyang <lhypds@gmail.com>
2023-11-05 09:42:34 +0900

    Only for full screen

--- pages/index.js
+++ pages/index.js
@@ -634,7 +634,12 @@ export default function Home() {
         if (placeholder.height)
           elInput.style.height = placeholder.height;
       }
-      document.documentElement.style.setProperty("--input-height", elInput.style.height);
+
+      // Store input height in fullscreen mode
+      // To calculate the height of output
+      if (isFullscreen) {
+        document.documentElement.style.setProperty("--input-height", elInput.style.height);
+      }
     }
   }
 
