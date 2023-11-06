a10f00c9df5678b262c911984dc0741ccc4c946d
LIU Heyang <lhypds@gmail.com>
2023-11-07 00:32:33 +0900

    Remove necessary function

--- utils/userUtils.js
+++ utils/userUtils.js
@@ -1,18 +1,3 @@
-export function generateSettings(format="json", role="", theme="light", speak="off", stats="on") {
-  let newSettings = {};
-  
-  // Default settings
-  newSettings["role"] = role;
-  newSettings["theme"] = theme;
-  newSettings["speak"] = speak;
-  newSettings["stats"] = stats;
-
-  if (format === "json") {
-    return JSON.stringify(newSettings);
-  }
-  return newSettings;
-}
-
 export function generatePassword(length=8) {
   var result = "";
   var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
