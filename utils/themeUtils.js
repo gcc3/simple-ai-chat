09b110b783b64b7fe63cfa9ed74dec25ba61e154
LIU Heyang <lhypds@gmail.com>
2023-11-06 14:55:58 +0900

    Add highlight for dark mode

--- utils/themeUtils.js
+++ utils/themeUtils.js
@@ -38,7 +38,7 @@ export function setTheme(theme) {
     document.documentElement.style.setProperty("--dot-hover-color", "#00f700");
     document.documentElement.style.setProperty("--sub-text-color", "#007000");
     document.documentElement.style.setProperty("--container-background-color", "rgba(0, 70, 0, 0.3)");
-    document.documentElement.style.setProperty("--strong-text-background-color", "transparent");
+    document.documentElement.style.setProperty("--strong-text-background-color", "rgba(0, 112, 0, 0.5)");
     document.documentElement.style.setProperty("--underline-text-background-color", "transparent");
     document.documentElement.style.setProperty("--underline-text-color", "#00f700");
   }
