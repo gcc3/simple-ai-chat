4732e883a6bff5764e283b2e7b7612943b9b109b
LIU Heyang <lhypds@gmail.com>
2023-11-10 05:04:21 +0900

    Small fix

--- utils/markdownUtils.js
+++ utils/markdownUtils.js
@@ -32,9 +32,6 @@ export function markdownFormatter(elOutput) {
       return codeBlocks[p1];
     });
 
-    // Replace the ```code_language_name to ```code to remove the language name
-    output = output.replace(/```(\w+)/g, '```');
-
     // Multi-line code blocks
     output = output.replace(/```([^`]+)```/g, `<pre>$1</pre>`);
 
@@ -44,7 +41,7 @@ export function markdownFormatter(elOutput) {
       .replace(/<\/pre><br><br>/g, '</pre><br>')   // Avoid consecutive breaks after </pre>
       .replace(/<br> ?<\/pre>/g, '</pre>')         // Remove <br> before </pre>
 
-    // Final lines replacer
+    // Replace **text** with <strong>text</strong>
     output = output.split('<br>').map((line, i) => {
       if (line.includes('**')) return line.replace(/\*\*(.*)\*\*/g, '<strong>$1</strong>');  // Replace **text** with text only
       return line;
