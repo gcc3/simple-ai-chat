11ec5fd7d06093e1c569baee59446e2ac138eef0
LIU Heyang <lhypds@gmail.com>
2023-11-10 11:23:11 +0900

    Separate inline code and pre

--- utils/markdownUtils.js
+++ utils/markdownUtils.js
@@ -19,14 +19,14 @@ export function markdownFormatter(elOutput) {
   elOutput.innerHTML = ((output) => {
     // Temporarily replace multi-line code blocks with placeholders
     const codeBlocks = [];
-    output = output.replace(/<pre>([^`]+)<\/pre>/g, (match, p1, offset, string) => {
-      codeBlocks.push(`<pre>${p1}</pre>`);
+    output = output.replace(/<pre><code>([^`]+)<\/code><\/pre>/g, (match, p1, offset, string) => {
+      codeBlocks.push(`<pre><code>${p1}</code></pre>`);
       return `###CODE###${codeBlocks.length - 1}`;
     });
 
     // Replace `text` with <code>text</code>
     output = output.split('<br>').map((line, i) => {
-      if (line.includes('`')) line = line.replace(/(?<!`)`([^`]+)`(?!`)/g, '<code>$1</code>');  // Inline code
+      if (line.includes('`')) line = line.replace(/(?<!`)`([^`]+)`(?!`)/g, '<code class="inline-code">$1</code>');  // Inline code
       if (line.includes('**')) line = line.replace(/\*\*(.*)\*\*/g, '<strong>$1</strong>');     // Replace **text** with text only
       return line;
     }).join('<br>');
@@ -42,10 +42,10 @@ export function markdownFormatter(elOutput) {
       if (line.trim().startsWith('```')) {
         if (!codeBlockOpen) {
           codeBlockOpen = true;
-          return line.trim().replace(/```/g, '<pre>');
+          return line.trim().replace(/```/g, '<pre><code>');
         } else if (codeBlockOpen) {
           codeBlockOpen = false;
-          return line.trim().replace(/```/g, '</pre>');
+          return line.trim().replace(/```/g, '</code></pre>');
         }
       }
       return line;
@@ -53,9 +53,9 @@ export function markdownFormatter(elOutput) {
 
     // Clean up <pre> tags
     output = output
-      .replace(/<pre>\s*(\w+)?\s*<br>/g, '<pre>')  // Remove language name followed by <br> after <pre>
-      .replace(/<\/pre><br><br>/g, '</pre><br>')   // Avoid consecutive breaks after </pre>
-      .replace(/<br> ?<\/pre>/g, '</pre>')         // Remove <br> before </pre>
+      .replace(/<pre><code>\s*(\w+)?\s*<br>/g, '<pre><code>')  // Remove language name followed by <br> after <pre>
+      .replace(/<\/pre><code><br><br>/g, '</code></pre><br>')   // Avoid consecutive breaks after </pre>
+      .replace(/<br> ?<\/code><\/pre>/g, '</code></pre>')         // Remove <br> before </pre>
 
     return output;
   })(output);
