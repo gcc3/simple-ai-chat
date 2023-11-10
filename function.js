43aa13074ffbc6d73b96d2b373152f2209395e06
LIU Heyang <lhypds@gmail.com>
2023-11-11 06:52:25 +0900

    Revert function calling and remove in non stream mode

--- function.js
+++ function.js
@@ -47,39 +47,33 @@ export function getFunctions() {
 
   // get time
   functions.push({
-    type: "function",
-    function: {
-      description: 'Provide the current time.',
-      name: 'get_time',
-      parameters: {
-        type: "object",
-        properties: {
-            timezone: {
-              type: "string",
-              description: "The timezone to get the time for. Use tz database timezone names. If unknown, the time will be in UTC.",
-            }
-        },
-        required: ["timezone"],
-      }
+    description: 'Provide the current time.',
+    name: 'get_time',
+    parameters: {
+      type: "object",
+      properties: {
+          timezone: {
+            type: "string",
+            description: "The timezone to get the time for. Use tz database timezone names. If unknown, the time will be in UTC.",
+          }
+      },
+      required: ["timezone"],
     }
   });
 
   // get weather
   functions.push({
-    type: "function",
-    function: {
-      description: 'Get weather for a given location or city, e.g. San Francisco, CA. Do not use it except user asks for it.',
-      name: 'get_weather',
-      parameters: {
-        type: "object",
-        properties: {
-          location: {
-            type: "string",
-            description: "The city and state, e.g. San Francisco, CA. If the city is not in English, translate it to English first.",
-          }
-        },
-        required: ["location"],
-      }
+    description: 'Get weather for a given location or city, e.g. San Francisco, CA. Do not use it except user asks for it.',
+    name: 'get_weather',
+    parameters: {
+      type: "object",
+      properties: {
+        location: {
+          type: "string",
+          description: "The city and state, e.g. San Francisco, CA. If the city is not in English, translate it to English first.",
+        }
+      },
+      required: ["location"],
     }
   });
 
@@ -87,20 +81,17 @@ export function getFunctions() {
   // only if AI node is enabled
   if (process.env.USE_NODE_AI === "true") {
     functions.push({
-      type: "function",
-      function: {
-        description: 'Get support or data or assistant from another AI if you totally do not know the answer.',
-        name: 'query_node_ai',
-        parameters: {
-          type: "object",
-          properties: {
-            query: {
-              type: "string",
-              description: "A question string. As you lack of some information or data, you need to ask another AI about that.",
-            }
-          },
-          required: ["query"],
-        }
+      description: 'Get support or data or assistant from another AI if you totally do not know the answer.',
+      name: 'query_node_ai',
+      parameters: {
+        type: "object",
+        properties: {
+          query: {
+            type: "string",
+            description: "A question string. As you lack of some information or data, you need to ask another AI about that.",
+          }
+        },
+        required: ["query"],
       }
     });
   }
@@ -109,20 +100,17 @@ export function getFunctions() {
   // only if Vectara is enabled
   if (process.env.USE_VECTOR === "true") {
     functions.push({
-      type: "function",
-      function: {
-        description: 'Get support data from vector database.',
-        name: 'query_vector',
-        parameters: {
-          type: "object",
-          properties: {
-            query: {
-              type: "string",
-              description: "A question string. As you lack of some information or data, you need to query from vector database.",
-            }
-          },
-          required: ["query"],
-        }
+      description: 'Get support data from vector database.',
+      name: 'query_vector',
+      parameters: {
+        type: "object",
+        properties: {
+          query: {
+            type: "string",
+            description: "A question string. As you lack of some information or data, you need to query from vector database.",
+          }
+        },
+        required: ["query"],
       }
     });
   }
--- pages/api/generate.js
+++ pages/api/generate.js
@@ -2,7 +2,6 @@ import OpenAI from "openai";
 import chalk from 'chalk';
 import { generateMessages } from "utils/promptUtils";
 import { logadd } from "utils/logUtils.js";
-import { getFunctions, executeFunction } from "function.js";
 import { getMaxTokens } from "utils/tokenUtils";
 
 // OpenAI
@@ -62,8 +61,6 @@ export default async function (req, res) {
     let result_text = "";
     let score = 0;
     let token_ct = 0;
-    let do_function_calling = false;
-    let function_name = "";
 
     const generateMessagesResult = await generateMessages(input, null, queryId, role);  // image_url not supported yet
     score = generateMessagesResult.score;
@@ -71,25 +68,13 @@ export default async function (req, res) {
 
     // endpoint: /v1/chat/completions
     let chatCompletion;
-    if (use_function_calling) {
-      chatCompletion = await openai.chat.completions.create({
-        model: process.env.MODEL,
-        messages: generateMessagesResult.messages,
-        temperature: temperature,
-        top_p: top_p,
-        max_tokens: max_tokens,
-        functions: getFunctions(),  // function calling
-        function_call: "auto"
-      });
-    } else {
-      chatCompletion = await openai.chat.completions.create({
-        model: process.env.MODEL,
-        messages: generateMessagesResult.messages,
-        temperature: temperature,
-        top_p: top_p,
-        max_tokens: max_tokens,
-      });
-    }
+    chatCompletion = await openai.chat.completions.create({
+      model: process.env.MODEL,
+      messages: generateMessagesResult.messages,
+      temperature: temperature,
+      top_p: top_p,
+      max_tokens: max_tokens,
+    });
 
     // Get result
     const choices = chatCompletion.data.choices;
@@ -99,59 +84,6 @@ export default async function (req, res) {
       result_text = "Silent...";
     } else {
       result_text = choices[0].message.content;
-
-      // Function call
-      if (use_function_calling && choices[0].message.function_call) {
-        console.log(chalk.cyanBright("Function calling (query_id = " + queryId + "):"));
-        do_function_calling = true;
-        const responseFunctionMessage = choices[0].message;
-
-        // Function name
-        const functionName = responseFunctionMessage.function_call.name;
-        function_name = functionName;
-        console.log("Function name: " + functionName);
-        
-        // Arguments
-        const args = JSON.parse(responseFunctionMessage.function_call.arguments);
-        let argsStrings = [];
-        for (const [key, value] of Object.entries(args)) {
-          console.log(key, value);
-          argsStrings.push(key + "=" + value);
-        }
-        const argsString = argsStrings.join(", ");
-        console.log("Arguments: " + argsString);
-
-        // Execute function
-        const functionResult = await executeFunction(functionName, argsString);
-        console.log("Result: " + functionResult.replace(/\n/g, "\\n") + "\n");
-
-        // Functions messages
-        // Include original messages
-        let functionMessages = generateMessagesResult.messages;
-        functionMessages.push(responseFunctionMessage);
-        functionMessages.push({
-          "role": "function",
-          "name": functionName,
-          "content": functionResult,
-        });
-
-        const functionChatCompletion = await openai.chat.completions.create({
-            model: process.env.MODEL,
-            messages: functionMessages,
-            temperature: temperature,
-            top_p: top_p,
-            max_tokens: max_tokens,
-        });
-
-        if (!choices || choices.length === 0) {
-          console.log(chalk.redBright("Error (query_id = " + queryId + "):"));
-          console.error("No choice\n");
-          result_text = "Silent...";
-        } else {
-          const functionResult = functionChatCompletion.choices[0].message.content;
-          result_text = functionResult;
-        }
-      }
     }
 
     // Output the result
@@ -168,7 +100,7 @@ export default async function (req, res) {
           top_p: process.env.TOP_P,
           score: score,
           token_ct: token_ct,
-          func: do_function_calling && function_name
+          func: false
         },
         info: {
           model: process.env.MODEL,
