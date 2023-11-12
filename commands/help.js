6705328de2fed746cc03462eddf6dd7a2346a9f5
LIU Heyang <lhypds@gmail.com>
2023-11-13 00:36:12 +0900

    Small change

--- commands/help.js
+++ commands/help.js
@@ -27,7 +27,7 @@ export default function help(args) {
     ":role [ls|list|reset] - List all roles, reset role.\n" +
     ":role use [role_name] - Use role.\n" +
     ":user add [username] [email?] - Create a user, with Email set.\n" +
-    ":user [del|delete] [username] - Delete user with all user's data.\n" +
+    ":user [del|delete] [username] - Delete user with data.\n" +
     ":user set [pass|password] [value] - Change password.\n" +
     ":user set [email] [value] - Change Email address.\n" +
     ":user info - Get logged-in user info and settings.\n" +
