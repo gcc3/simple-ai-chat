c33c03471a164ced39920274cd9ce0ab78ab3214
LIU Heyang <lhypds@gmail.com>
2023-11-15 23:18:27 +0900

    Small change

--- utils/markdownUtils.js
+++ utils/markdownUtils.js
@@ -77,9 +77,14 @@ export function markdownFormatter(elOutput) {
 
     // Set language name and highlight code blocks
     output = output.replace(/<pre><code>([^`]+)<\/code><\/pre>/g, (match, p1) => {
-      let languageName = p1.split('\n')[0].trim();
+      let codeLines = p1.split('\n');
+
+      // Get the language name and code
+      let languageName = codeLines[0].trim();
       if (!languageName) languageName = 'plaintext';
-      const code = p1.split('\n').slice(1).join('\n').trim();
+      const code = codeLines.slice(1).join('\n');
+
+      // Highlight the code
       return `<pre><code class="code-block !whitespace-pre hljs language-${languageName}">${code}</code></pre>`;
     });
 
