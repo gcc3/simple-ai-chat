c108320864fd1a0086e2f53c8607591aa0ffd4f1
LIU Heyang <lhypds@gmail.com>
2023-12-05 11:08:54 +0900

    Disable temporarily

--- components/Subscription.js
+++ components/Subscription.js
@@ -3,13 +3,14 @@ import PayPalButton from "./PayPalButton";
 function Subscription() {
   const content = (
     <div>
-      <div>Subscribe to become a pro/super user, $5/momth.</div>
+      <div>Payment feature is in working progress, please contect support@simple-ai.io, to get a super_user account.</div>
+      {/* <div>Subscribe to become a pro/super user.</div>
       <div className="mt-3 max-w-md">
         <PayPalButton onSuccess={(details) => {
           console.log("Transaction completed by " + details.payer.name);
           console.log("Detail: ", details);
         }} />
-      </div>
+      </div> */}
     </div>
   )
 
