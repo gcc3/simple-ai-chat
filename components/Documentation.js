012188d6d1c0b6b816924340c464700f11a68ec9
LIU Heyang <lhypds@gmail.com>
2023-12-06 13:02:56 +0900

    Documentation section

---
+++ components/Documentation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Documentation = () => {
+  const content = (
+    <>
+      <div>Building in progress...</div>
+    </>
+  )
+
+  return (
+    <div className="Documentation">
+      <div className="text-center mb-4">
+        <div>Welcome to the simple-ai.io</div>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
+};
+
+export default Documentation;
