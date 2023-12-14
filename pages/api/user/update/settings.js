574756cfbe13cb73382a9736a9cb4fea42b50819
LIU Heyang <lhypds@gmail.com>
2023-12-14 13:47:47 +0900

    Small change

--- pages/api/user/update/settings.js
+++ pages/api/user/update/settings.js
@@ -40,7 +40,7 @@ export default async function (req, res) {
     if (wasSuccessful) {
       return res.status(200).json({ 
         success: true, 
-        message: "Settings updated successfully" 
+        message: "Settings updated successfully."
       });
     } else {
       return res.status(400).json({ 
