c378d5efafb47e4ce0a2a3f78734aaf062a87e08
LIU Heyang <lhypds@gmail.com>
2023-12-14 21:43:23 +0900

    Group stores

--- pages/api/store/list.js
+++ pages/api/store/list.js
@@ -1,23 +1,36 @@
 import { authenticate } from 'utils/authUtils';
-import { getUserStores } from 'utils/sqliteUtils';
+import { getUser, getUserStores } from 'utils/sqliteUtils';
 
 export default async function (req, res) {
   try {
     // User stores
     let userStores = [];
+    let stores = [];
     const authResult = authenticate(req);
     if (authResult.success) {
       const { id, username } = authResult.user;
 
       // Get user stores
-      userStores = await getUserStores(username);
+      userStores.push(...await getUserStores(username));
+
+      // Get group stores
+      const user = await getUser(username);
+      const groups = user.group.split(',');
+      for (const group of groups) {
+        if (!group || group === user.username) {
+          continue;
+        }
+
+        const groupStores = await getUserStores(group);
+        stores.push(...groupStores);
+      }
     }
 
     // Output the result
     res.status(200).json({
       result: {
         user_stores: userStores,
-        stores: [],  // default stores
+        stores: stores,  // group stores
       },
     });
   } catch (error) {
