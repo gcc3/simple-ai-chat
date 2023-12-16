613613909b7803f4a433e29587dbadf522676279
LIU Heyang <lhypds@gmail.com>
2023-12-17 02:01:46 +0900

    Fix root user cannot access

--- utils/uacUtils.js
+++ utils/uacUtils.js
@@ -64,7 +64,7 @@ export async function getUacResult(user, ip) {
 }
 
 async function checkFequenciesExceeded(user) {
-  if (user.role == "root") {
+  if (user.role == "root_user") {
     return false;
   }
 
