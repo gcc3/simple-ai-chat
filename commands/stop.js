461336396d1e88d161237f2e506f914bdbd8579b
LIU Heyang <lhypds@gmail.com>
2023-12-17 15:32:56 +0900

    Small change

--- commands/stop.js
+++ commands/stop.js
@@ -1,6 +1,7 @@
 const STATES = { IDLE: 0, DOING: 1 };
 
 export default function stop(args) {
+  console.log("Stopping...");
   global.STATE = STATES.IDLE;  // global is bad, but it's a quick hack
   window.speechSynthesis.cancel();
   return null;
