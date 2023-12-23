1f8049cfee3887d0d47b211d16d653efda10ddcd
LIU Heyang <lhypds@gmail.com>
2023-12-24 01:26:21 +0900

    Small change

--- functions/redirect_to_url.js
+++ functions/redirect_to_url.js
@@ -33,7 +33,7 @@ export default async function redirectToUrl(paramObject) {
   if (blank) blankMessage = " Brower opened URL in a new tab";
   return {
     success: true,
-    message: "Browser is redirected to `" + url + "`." + blankMessage + " Only redirect once no metter success or failure.",  // It's actually redirecting but AI required to be told it is redirected
+    message: "Browser is redirected to `" + url + "`." + blankMessage,
     event: { name: "redirect", parameters: { url, blank }, },
   };
 }
