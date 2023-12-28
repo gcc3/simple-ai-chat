69e2424b00dce5e8a73b24ab381f756c7e2ada27
LIU Heyang <lhypds@gmail.com>
2023-12-29 00:36:50 +0900

    Store init command

--- pages/api/store/init.js
+++ pages/api/store/init.js
@@ -1,6 +1,7 @@
 import { getUser, getStore, updateStoreEngine, updateStoreSettings } from "utils/sqliteUtils.js";
 import { authenticate } from "utils/authUtils.js";
 import { createVectaraCorpus, generateVectaraApiKey, createVectaraJtwToken } from "utils/vectaraUtils.js";
+import { mysqlQuery } from "utils/mysqlUtils.js";
 
 export default async function (req, res) {
   // Check if the method is POST
@@ -8,7 +9,7 @@ export default async function (req, res) {
     return res.status(405).end();
   }
 
-  const { name, engine } = req.body;
+  const { name } = req.body;
 
   // Authentication
   const authResult = authenticate(req);
@@ -31,25 +32,17 @@ export default async function (req, res) {
   }
 
   // Check engine existance
-  if (!engine) {
+  if (!store.engine) {
     return res.status(400).json({ 
       success: false, 
       error: "Engine not specified." 
     });
   }
 
-  // Check store settings existance
-  if (store.engine) {
-    return res.status(400).json({ 
-      success: false, 
-      error: "Store already initialized." 
-    });
-  }
-
   console.log("Initializing store \"" + name + "\"...");
 
   if (engine === "vectara") {
-    const initResult = await initializeVectaraStore();
+    const initResult = await initializeVectaraStore(store);
     if (!initResult.success) {
       return res.status(400).json({ 
         success: false, 
@@ -67,7 +60,7 @@ export default async function (req, res) {
   }
 
   if (engine === "mysql") {
-    const initResult = await initializeMysqlStore();
+    const initResult = await initializeMysqlStore(store);
     if (!initResult.success) {
       return res.status(400).json({ 
         success: false, 
@@ -91,24 +84,89 @@ export default async function (req, res) {
 }
 
 async function initializeMysqlStore() {
+  let settings = JSON.parse(store.settings);
+
+  if (!settings.host || !settings.port || !settings.user || !settings.password || !settings.database) {
+    return {
+      success: false,
+      error: "Host, port, user, password, database are required."
+    };
+  }
+
+  const dbConfig = {
+    host: settings.host,
+    port: settings.port,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    table: settings.table,
+  };
+
+  // Generate database schema string
+  let database_schema_string = "";
+  let databaseSchemas = [];
+
+  // Get table names
+  const tableNames = await mysqlQuery(dbConfig, "SHOW TABLES");
+  if (!tableNames) {
+    return {
+      success: false,
+      error: "Failed to get table names."
+    };
+  }
+
+  // Get column names
+  for (const tableName of tableNames) {
+    const table = tableName[Object.keys(tableName)[0]];
+    if (dbConfig.table && dbConfig.table !== table) {
+      // Skip if table name is specified and not match
+      continue;
+    }
+
+    // Get column names
+    const columnNames = await mysqlQuery(dbConfig, `SHOW COLUMNS FROM ${table}`);
+    if (!columnNames) {
+      return {
+        success: false,
+        error: "Failed to get column names in table \"" + table + "\"."
+      };
+    }
+
+    const columnNamesArray = columnNames.map(columnName => columnName.Field);
+    databaseSchemas.push({ "table_name": table, "column_names": columnNamesArray });
+  }
+
+  // Generate table descriptions and database schema string
+  const tableDescriptions = databaseSchemas.map(table => {
+    return `Table: ${table.table_name}\nColumns: ${table.column_names.join(', ')}`;
+  });
+  database_schema_string = tableDescriptions.join('\n\n');
+
+  settings = {
+    ...settings,
+    schema: database_schema_string,
+  };
+
   return {
     success: true,
-    settings: {
-      host: "",
-      port: 3306,
-      user: "",
-      password: "",
-      database: "",
-      table: "",
-      tableColumnsDef: "",
-      description: "",
-    }
+    settings: settings
   }
 }
 
-async function initializeVectaraStore() {
+async function initializeVectaraStore(store) {
+  let settings = JSON.parse(store.settings);
+
+  if (!settings.vectaraApiKey || !settings.vectaraCustomerId || !settings.vectaraClientId || !settings.vectaraClientSecret) {
+    return { 
+      success: false, 
+      error: "Vectara API key, customer ID, client ID and client secret are required."
+    };
+  }
+
+  const { vectaraApiKey, vectaraCustomerId, vectaraClientId, vectaraClientSecret } = settings;
+
   // Get JWT token
-  const jwtToken = await createVectaraJtwToken();
+  const jwtToken = await createVectaraJtwToken(vectaraClientId, vectaraClientSecret, vectaraCustomerId, vectaraApiKey);
   if (!jwtToken) {
     console.log("Failed to get JTW token.");
     return { 
@@ -122,7 +180,7 @@ async function initializeVectaraStore() {
   // Create store
   const corpusName = "i-" + Date.now();
   const description = "store: " + name + ", created by: " + username;
-  const corpusId = await createVectaraCorpus(corpusName, description, jwtToken);
+  const corpusId = await createVectaraCorpus(corpusName, description, jwtToken, vectaraCustomerId);
   if (!corpusId) {
     console.log("Failed to create corpus.");
     return { 
@@ -134,7 +192,7 @@ async function initializeVectaraStore() {
   console.log("Created corpus: " + corpusId);
 
   // Get API key
-  const apiKey = await generateVectaraApiKey(corpusId, jwtToken);
+  const apiKey = await generateVectaraApiKey(corpusId, jwtToken, vectaraCustomerId);
   if (!apiKey) {
     console.log("Failed to get API key.");
     return { 
@@ -145,7 +203,8 @@ async function initializeVectaraStore() {
 
   console.log("Got API key.");
 
-  const settings = {
+  settings = {
+    ...settings,
     corpusId: corpusId,
     apiKey: apiKey,
     threshold: 0.6,
