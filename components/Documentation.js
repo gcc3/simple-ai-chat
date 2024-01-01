bb5ec0410f8c93a3637cd2948ecb09c54713e5fa
LIU Heyang <lhypds@gmail.com>
2024-01-01 13:57:23 +0900

    Update doc

--- components/Documentation.js
+++ components/Documentation.js
@@ -168,7 +168,7 @@ const Documentation = () => {
           </tbody>
         </table>
         <div className="mt-2">
-          * Command shortcuts: Stop generating (⌃c), Clear output (⌃r), Clear output and reset session (⇧⌃r), Toggle fullscreen (F11) or split mode (⌃|).
+          * Command shortcuts: Stop generating (⌃c), Clear output (⌃r), Clear output and reset session (⇧⌃r), Toggle fullscreen (F11) or split mode (⌃|). In macOS you may use ⌃F11 because F11 is a system shortcut.
         </div>
       </div>
       <div id="api" className="mt-5">APIs</div>
