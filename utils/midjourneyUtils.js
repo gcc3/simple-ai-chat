a9ca976c8a73bd5a4ccc0e3225e63f00a5e94f73
LIU Heyang <lhypds@gmail.com>
2024-01-03 03:37:58 +0900

    User add new description

--- utils/midjourneyUtils.js
+++ utils/midjourneyUtils.js
@@ -3,32 +3,40 @@ import OpenAI from "openai";
 // OpenAI
 const openai = new OpenAI();
 
-export async function generateMidjourneyPrompt(input) {
+export async function generateMidjourneyPrompt(input, lastMjPrompt) {
   if (!input || input.trim().length === 0) {
     return null;
   }
 
+  if (lastMjPrompt && lastMjPrompt.trim().length > 0) {
+    input = "Last time you generated prompt: " + lastMjPrompt + "\n" 
+          + "I want you modify it to fit my new requirement: " + input;
+  }
+
+  let prompt = "You are an awesome Midjourney prompt generator. " 
+             + "User provides you, what they desired picture description. You'll add your imagination on it." + "\n"
+             + "User may provide you the last Midjourney prompt, you will need to modify it base on a new requirement." + "\n"
+             + "The input maybe not English, please translate and always use English as prompt." + "\n"
+             + "You'll generate a valid Midjourney prompt with parameters (optional) and response with JSON format below: \n\n"
+             + "{" + "\n"
+             + "  \"prompt\": \"AWESOME_MIDJOURNEY_PROMPT\"" + "\n"
+             + "}" + "\n\n"
+             + "The prompt must be written before any parameters." + "\n"
+             + "Parameters example: \"--ar 2:3 --no sky\" \n\n"
+             + "Available parameters: --ar : Change the aspect ratio of a generation.\n"
+             + "                      --no : Negative prompting, --no plants would try to remove plants from the image.\n"
+             + "                      --chaos <number 0–100> Change how varied the results will be. Higher values produce more unusual and unexpected generations.\n"
+             + "                      --iw <0–2> Sets image prompt weight relative to text weight. The default value is 1.\n"
+             + "If you know how to use Midjourney, please add your parameters to the prompt. \n"
+             + "Remember if user asked a question not related to image generation, you should response an empty query as follows: \n\n"
+             + "{" + "\n"
+             + "  \"prompt\": \"\"" + "\n"
+             + "}" + "\n\n"
+
   let messages = [];
   messages.push({ 
     role: "system",
-    content: "You are an awesome Midjourney prompt generator. " 
-           + "User provides you, what they desired picture description. You'll add your imagination on it." + "\n"
-           + "The input maybe not English, please translate and always use English as prompt." + "\n"
-           + "You'll generate a valid Midjourney prompt with parameters (optional) and response with JSON format below: \n\n"
-           + "{" + "\n"
-           + "  \"prompt\": \"AWESOME_MIDJOURNEY_PROMPT\"" + "\n"
-           + "}" + "\n\n"
-           + "The prompt must be written before any parameters." + "\n"
-           + "Parameters example: \"--ar 2:3 --no sky\" \n\n"
-           + "Available parameters: --ar : Change the aspect ratio of a generation.\n"
-           + "                      --no : Negative prompting, --no plants would try to remove plants from the image.\n"
-           + "                      --chaos <number 0–100> Change how varied the results will be. Higher values produce more unusual and unexpected generations.\n"
-           + "                      --iw <0–2> Sets image prompt weight relative to text weight. The default value is 1.\n"
-           + "If you know how to use Midjourney, please add your parameters to the prompt. \n"
-           + "Remember if user asked a question not related to image generation, you should response an empty query as follows: \n\n"
-           + "{" + "\n"
-           + "  \"prompt\": \"\"" + "\n"
-           + "}" + "\n\n"
+    content: prompt
   });
 
   messages.push({
