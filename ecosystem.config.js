146b0797cdc61fd083b418204a90f3ebf2b6a572
LIU Heyang <lhypds@gmail.com>
2024-01-03 01:22:20 +0900

    Miss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps : [{
     name: 'simple-ai.io',
-    script: 'app.js',
-    args: 'run',
+    script: 'npm',
+    args: 'start',
     interpreter: '/root/.nvm/versions/node/v21.5.0/bin/node',
     // watch: true,   // restart on file changes
     env: {
