ef5a02a06b21b7080794b5fb27c6db4e660ad9ae
LIU Heyang <lhypds@gmail.com>
2024-01-02 23:12:14 +0900

    Remove log filters

--- utils/logUtils.js
+++ utils/logUtils.js
@@ -1,18 +1,9 @@
+import { isPageStatic } from "next/dist/build/utils.js";
 import { getLogs, insertLog } from "./sqliteUtils.js"
 
 const fs = require('fs');
 
 export async function logadd(user, session, model, input_token_ct, input, output_token_ct, output, images, ip, browser) {
-  // Filter out logs
-  if (logfilter(output, "USER")) {
-    console.log("Log ignored (USER).\n");
-    return;
-  }
-  if (logfilter(output, "IP")) {
-    console.log("Log ignored (IP).\n");
-    return;
-  }
-
   // Get username
   let username = "";
   if (user) {
@@ -27,19 +18,4 @@ export async function loglist(session, limit = 50) {
   let loglines = "";
   if (!session) return loglines;  // don't show anything if no session is given
   return await getLogs(session, limit);
-}
-
-function logfilter(line, indicater) {
-  // filter with rules
-  const rules = fs.readFileSync('./log.config', 'utf8').split("\n");
-  for (const rule of rules) {
-    if (rule.startsWith(indicater)) {
-      const ruleValue = rule.substring(indicater.length + 1).trim();
-      if (line.includes(ruleValue)) {
-        console.log("Log ignored (" + indicater + " = " + ruleValue + ").\n");
-        return true;
-      }
-    }
-  }
-  return false;
 }
