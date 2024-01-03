42569c0eaac0b47b7d98eea0ae5e0032e55e6731
LIU Heyang <lhypds@gmail.com>
2024-01-03 16:17:31 +0900

    Small change

--- pages/api/user/add.js
+++ pages/api/user/add.js
@@ -55,7 +55,7 @@ export default async function (req, res) {
   // Check if the IP alread used for another user
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const countUserWithSameIP = await countUserByIP(ip);
-  if (countUserWithSameIP > 0) {
+  if (countUserWithSameIP >= 3) {
     return res.status(400).json({
       success: false,
       error: "Your IP address has been used too frequently. For assistance, please contact our support at `support@simple-ai.io`.",
