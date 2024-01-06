4d8a3f8ea10c1ba3b462a588678dd15916b3f881
LIU Heyang <lhypds@gmail.com>
2024-01-06 14:36:35 +0900

    Update fee

--- components/SubscriptionComparisonTable.js
+++ components/SubscriptionComparisonTable.js
@@ -7,26 +7,20 @@ function getPriceString(subscriptions, role) {
     if (subscriptions[role].price == 0) {
       price = "Free";
     }
-
-    if (role === "user")
-       price = "$" + subscriptions[role].price + "/month+(usage fee +5%)";
-    else if (role === "pro_user")
-      price = "$" + subscriptions[role].price + "/month+(usage fee +3%)";
-    else if (role === "super_user")
-      price = "$" + subscriptions[role].price + "/month+(usage fee +1%)";
+    price = "$" + subscriptions[role].price + "/month +usage fee(+1%)";
   }
   return price;
 }
 
 const SubscriptionComparisonTable = ({ subscriptions }) => {
   const comparison = [
-    { name: "GPT-4 Turbo", user: "100/day", pro_user: "200/day", super_user: "300/day" },
+    { name: "GPT-4 Turbo", user: "120/day", pro_user: "500/day", super_user: "No limit" },
     { name: "GPT-4 Vision (Image Input)", user: "No limit", pro_user: "No limit", super_user: "No limit" },
     { name: "File Input", user: "Yes", pro_user: "Yes", super_user: "Yes" },
     { name: "Role", user: "Yes", pro_user: "Yes", super_user: "Yes" },
     { name: "Data Store", user: "Yes", pro_user: "Yes +support", super_user: "Yes +support" },
     { name: "Node (Node AI)", user: "Yes", pro_user: "Yes", super_user: "Yes +support" },
-    { name: "Midjourney", user: "╳", pro_user: "20/day", super_user: "50/day" },
+    { name: "Midjourney", user: "1/day(trial)", pro_user: "20/day", super_user: "50/day +fast" },
     { name: "Pricing", user: getPriceString(subscriptions, "user"), pro_user: getPriceString(subscriptions, "pro_user"), super_user: getPriceString(subscriptions, "super_user")},
   ];
 
