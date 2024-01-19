670804b15cf0d44329a95750e66e20e7157425b7
LIU Heyang <lhypds@gmail.com>
2024-01-20 05:57:34 +0900

    List parent session log

--- utils/logUtils.js
+++ utils/logUtils.js
@@ -1,4 +1,4 @@
-import { getLogs, getLog, insertLog, insertSession } from "./sqliteUtils.js"
+import { getLogs, getSession, getLog, insertLog, insertSession } from "./sqliteUtils.js"
 
 export async function logadd(user, session, time, model, input_token_ct, input, output_token_ct, output, images, ip, browser) {
   // Get username
@@ -27,8 +27,39 @@ export async function logadd(user, session, time, model, input_token_ct, input, 
   await insertLog(session, time, username, model, input_token_ct, input, output_token_ct, output, images, ip, browser);
 }
 
-export async function loglist(session, limit = 50) {
-  let loglines = "";
-  if (!session) return loglines;  // don't show anything if no session is given
-  return await getLogs(session, limit);
+export async function loglist(initId, limit = 50) {
+  let loglines = [];
+
+  // Get current session logs
+  let session = await getSession(initId);
+  if (!session) {
+    return loglines;
+  }
+  loglines = await getLogs(initId, limit);
+
+  if (loglines.length >= limit) {
+    // Limit reached, return
+    return loglines;
+  }
+
+  // Get parent session logs
+  while (session) {
+    if (session.id == session.parent_id) {
+      // Root session, break
+      break;
+    }
+
+    if (loglines.length >= limit) {
+      // Limit reached, break
+      break;
+    }
+
+    // Go to parent session
+    session = await getSession(session.parent_id);
+
+    // Get parent session logs
+    const logs = await getLogs(session.id, limit);
+    loglines = logs.concat(loglines);
+  }
+  return 
 }
