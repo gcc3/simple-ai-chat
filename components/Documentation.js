85e2ba376b76a3d18c6968c0898c5b6e4e158953
LIU Heyang <lhypds@gmail.com>
2024-01-22 00:25:49 +0900

    Small fix

--- components/Documentation.js
+++ components/Documentation.js
@@ -319,21 +319,21 @@ const Documentation = ({ country }) => {
       </div>
       <div id="feedback" className="mt-5">{ t("Feedback & Support") }</div>
       <div className="mt-2">
-        <Trans
+        - <Trans
           i18nKey="discord_server"
           components={{ 1: <a href="https://discord.gg/sRcj4HZyzU" target="_blank" rel="noopener noreferrer">{ t('link') }</a>, 2: <u></u> }}
           ns="documentation"
         />
       </div>
-      <div className="mt-2">
-        <Trans
+      <div>
+        - <Trans
           i18nKey="email_support"
           components={{ 1: <a href="mailto:support@simple-ai.io" target="_blank" rel="noopener noreferrer">{ t('link') }</a>, 2: <u></u> }}
           ns="documentation"
         />
       </div>
-      <div className="mt-2">
-        <Trans
+      <div>
+        - <Trans
           i18nKey="github_issues"
           components={{ 1: <a href="https://github.com/gcc3/simple-ai-chat/issues" target="_blank" rel="noopener noreferrer">{ t('link') }</a>, 2: <u></u> }}
           ns="documentation"
