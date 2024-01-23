cd1fcdede8d65f77a91a168050938a689eac08d6
LIU Heyang <lhypds@gmail.com>
2024-01-24 01:21:02 +0900

    Small fix

--- components/Documentation.js
+++ components/Documentation.js
@@ -217,7 +217,7 @@ const Documentation = ({ country }) => {
           <tbody>
             {functions.map((f, index) => (
               <tr key={index}>
-                <td>{f.name}</td>
+                <td>{f.name}<br></br> {"(" + f.friendly_name + ")"}</td>
                 <td>{(() => {
                   const args =(() => Object.keys(f.parameters.properties).map((p) => {
                     const type = f.parameters.properties[p].type;
