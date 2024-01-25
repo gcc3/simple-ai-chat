70f8486ab0805f8f80e2b8dae79ebaea55fbf8c0
LIU Heyang <lhypds@gmail.com>
2024-01-25 12:39:21 +0900

    Small fix

--- components/Usage.js
+++ components/Usage.js
@@ -116,7 +116,7 @@ function Usage() {
 
   const content = (
     <>
-      {!user && <div>{ t("Please login. To register a user, use the command `:user add [username] [email] [password?]` (no brackets needed).") }</div>}
+      {!user && <div>{ t("User infromation not found. Please login with command `:login [username] [password]`.") }</div>}
       {user && <div>
         <div>
           <div className="mb-1">- { t("Subcription Status") }</div>
