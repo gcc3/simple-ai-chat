c82199866d9452b3e308259c73b47f0a0ae67ff1
LIU Heyang <lhypds@gmail.com>
2024-01-28 00:52:16 +0900

    Translate subscription table

--- components/SubscriptionComparisonTable.js
+++ components/SubscriptionComparisonTable.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { getRoleFequencyLimit } from "utils/usageUtils";
+import { useTranslation, Trans } from "react-i18next";
 
 // Get amount
-function getPriceString(subscriptions, role) {
+function getPriceString(subscriptions, role, t) {
   let price = "";
   if (subscriptions.hasOwnProperty(role)) {
-    price = (Number(subscriptions[role].price) === 0 ? "Free" : "$" + subscriptions[role].price + "/month") + " +usage_fee";
+    price = (Number(subscriptions[role].price) === 0 ? "Free" : "$" + subscriptions[role].price + "/month") + " +" + t("Usage Fee");
   }
   return price;
 }
@@ -16,29 +17,31 @@ function getRoleFequencyLimits(role) {
 }
 
 const SubscriptionComparisonTable = ({ subscriptions }) => {
+  const { t } = useTranslation("subscriptions");
+
   const comparison = [
-    { name: "Text Generation (GPT-4 Turbo)", user: getRoleFequencyLimits(`user`), pro_user: getRoleFequencyLimits(`pro_user`), super_user: getRoleFequencyLimits(`super_user`) },
-    { name: "Image Generation (Midjourney)", user: "1/day(trial)", pro_user: "20/day", super_user: "50/day +fast" },
-    { name: "Image Input (GPT-4 Vision)", user: "No limit", pro_user: "No limit", super_user: "No limit" },
-    { name: "File Input", user: "Yes", pro_user: "Yes", super_user: "Yes" },
-    { name: "Roles", user: "Yes", pro_user: "Yes", super_user: "Yes" },
-    { name: "Data Stores", user: "Yes", pro_user: "Yes +support", super_user: "Yes +support" },
-    { name: "Nodes (Node AI)", user: "Yes", pro_user: "Yes", super_user: "Yes +support" },
-    { name: "Enhanced Knowledge & Mathematics (WolframAlpha)", user: "Yes(trial)", pro_user: "Yes", super_user: "Yes" },
-    { name: "Pricing", user: getPriceString(subscriptions, "user"), pro_user: getPriceString(subscriptions, "pro_user"), super_user: getPriceString(subscriptions, "super_user")},
+    { name: t("Text Generation (GPT-4 Turbo)"), user: getRoleFequencyLimits(`user`), pro_user: getRoleFequencyLimits(`pro_user`), super_user: getRoleFequencyLimits(`super_user`) },
+    { name: t("Image Generation (Midjourney)"), user: t("1/day(trial)"), pro_user: t("20/day"), super_user: t("50/day +fast") },
+    { name: t("Image Input (GPT-4 Vision)"), user: t("No limit"), pro_user: t("No limit"), super_user: t("No limit") },
+    { name: t("File Input"), user: t("Yes"), pro_user: t("Yes"), super_user: t("Yes") },
+    { name: t("Roles"), user: t("Yes"), pro_user: t("Yes"), super_user: "Yes" },
+    { name: t("Data Stores"), user: t("Yes"), pro_user: t("Yes +support"), super_user: t("Yes +support") },
+    { name: t("Nodes (Node AI)"), user: t("Yes"), pro_user: t("Yes"), super_user: t("Yes +support") },
+    { name: t("Enhanced Knowledge & Mathematics (WolframAlpha)"), user: t("Yes(trial)"), pro_user: t("Yes"), super_user: t("Yes") },
+    { name: t("Pricing"), user: getPriceString(subscriptions, "user", t), pro_user: getPriceString(subscriptions, "pro_user", t), super_user: getPriceString(subscriptions, "super_user", t)},
   ];
 
   return (
     <div className="mt-3">
       <div className="mb-1">
-        <div>Features Comparison and Pricing</div>
+        <div>{ t("Features Comparison and Pricing") }</div>
       </div>
       <div className="table-container">
         <table className="table-fixed">
           <thead>
             <tr>
-              <th rowSpan="2">Features</th>
-              <th colSpan="3">Subcriptions</th>
+              <th rowSpan="2">{ t("Features") }</th>
+              <th colSpan="3">{ t("Subcriptions") }</th>
             </tr>
             <tr>
               <th>`user`</th>
@@ -58,8 +61,14 @@ const SubscriptionComparisonTable = ({ subscriptions }) => {
           </tbody>
         </table>
       </div>
-      <div className="mt-2">* For details of the usage fee, please refer to the Usage page.</div>
-      <div>* For enterprise use, please contact us at `<a href="mailto:support@simple-ai.io"><u>support@simple-ai.io</u></a>`.</div>
+      <div className="mt-2">{ t("* For details of the usage fee, please refer to the Usage page.") }</div>
+      <div>
+        <Trans
+          i18nKey="email_support_subscriptions"
+          components={{ 1: <a href="mailto:support@simple-ai.io" target="_blank" rel="noopener noreferrer">{ t('link') }</a>, 2: <u></u> }}
+          ns="subscriptions"
+        />
+      </div>
     </div>
   );
 };
