c92d2e54f7bcf916ebfdcd55cedb1063e5e8b75f
Gee Chai <fc00004@gmail.com>
2024-01-29 12:18:15 +0900

    fix: minor text typo correction

--- components/Subscription.js
+++ components/Subscription.js
@@ -113,7 +113,7 @@ function Subscription() {
         <div className="mt-1">{ t("User") }: {user.username}</div>
         <div>{ t("Email") }: {user.email}</div>
         <div>{ t("Subscription") }: `{user.role}`</div>
-        <div>{ t("Expire at") }: {user.role_expires_at ? moment.unix(user.role_expires_at / 1000).format('MM/DD/YYYY') : "(unlimit)"} {(user.role_expires_at && user.role_expires_at < new Date()) && "(Expired)"}</div>
+        <div>{ t("Expire at") }: {user.role_expires_at ? moment.unix(user.role_expires_at / 1000).format('MM/DD/YYYY') : "(unlimited)"} {(user.role_expires_at && user.role_expires_at < new Date()) && "(Expired)"}</div>
       </div>}
       <div className="mt-3">
         <div>- { t("Subscription plans") }</div>
