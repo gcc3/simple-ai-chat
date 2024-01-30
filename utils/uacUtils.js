85867d9b0b0852a4152b1fda6592b4f73da2e32f
LIU Heyang <lhypds@gmail.com>
2024-01-31 08:59:09 +0900

    Small change

--- utils/uacUtils.js
+++ utils/uacUtils.js
@@ -16,7 +16,7 @@ export async function getUacResult(user, ip) {
     if (chatCount >= 6) {
       return {
         success: false,
-        error: "To proceed, kindly sign up by creating a new `user` account. Registration is simple—just enter the command `:user add [username] [email] [password?]`, omitting the brackets and substituting in your own details for username, email, and password. Remember that the username should only contain letters and not begin with a number. For instance, you can type `:user add john xxxx@zzzz.vv ********` to register. Existing members, please log in with `:user login [username] [password]`. Please note, all commands should start with a half-width colon (:).<br><br>"
+        error: "To proceed, kindly sign up by creating a new `user` account. Registration is simple—just enter the command `:user add [username] [email] [password?]`, omitting the brackets and substituting in your own details for username, email, and password. Remember that the username should only contain letters and not begin with a number. For instance, you can type `:user add john xxxx@zzzz.vv ********` to register. Existing members, please log in with `:user login [username] [password]`. Please note, all commands should start with a half-width colon (:)."
       };
     }
   }
