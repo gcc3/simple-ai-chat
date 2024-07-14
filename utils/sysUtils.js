b06050fc587461052dcc5627ccc1f3146657b496
LIU Heyang <lhypds@gmail.com>
2024-07-14 15:57:22 +0900

    Small fix

--- utils/sysUtils.js
+++ utils/sysUtils.js
@@ -4,21 +4,21 @@ export function getSystemConfigurations() {
   const model = process.env.MODEL ? process.env.MODEL : "";
   const model_v = process.env.MODEL_V ? process.env.MODEL_V : "";
   const role_content_system = process.env.ROLE_CONTENT_SYSTEM ? process.env.ROLE_CONTENT_SYSTEM : "";
-  const temperature = process.env.TEMPERATURE ? Number(process.env.TEMPERATURE) : 0.7;  // default is 0.7
-  const top_p = process.env.TOP_P ? Number(process.env.TOP_P) : 1;                      // default is 1
-  const init_placeholder = process.env.INIT_PLACEHOLDER ? process.env.INIT_PLACEHOLDER : "";
-  const waiting = process.env.WAITING ? process.env.WAITING : "";
+  const welcome_message = process.env.WELCOME_MESSAGE ? process.env.WELCOME_MESSAGE : "";
+  const querying = process.env.QUERYING ? process.env.QUERYING : "Querying...";
   const generating = process.env.GENERATING ? process.env.GENERATING : "";
   const searching = process.env.SEARCHING ? process.env.SEARCHING : "";
-  const querying = process.env.QUERYING ? process.env.QUERYING : "Querying...";
+  const waiting = process.env.WAITING ? process.env.WAITING : "";
+  const init_placeholder = process.env.INIT_PLACEHOLDER ? process.env.INIT_PLACEHOLDER : "";
   const enter = process.env.ENTER ? process.env.ENTER : "";
+  const temperature = process.env.TEMPERATURE ? Number(process.env.TEMPERATURE) : 0.7;  // default is 0.7
+  const top_p = process.env.TOP_P ? Number(process.env.TOP_P) : 1;                      // default is 1
   const max_tokens = process.env.MAX_TOKENS ? Number(process.env.MAX_TOKENS) : getMaxTokens(model);
   const use_function_calling = process.env.USE_FUNCTION_CALLING == "true" ? true : false;
   const use_node_ai = process.env.USE_NODE_AI == "true" ? true : false;
   const use_payment = process.env.USE_PAYMENT == "true" ? true : false;
   const use_access_control = process.env.USE_ACCESS_CONTROL == "true" ? true : false;
   const use_email = process.env.USE_EMAIL == "true" ? true : false;
-  const welcome_message = process.env.WELCOME_MESSAGE ? process.env.WELCOME_MESSAGE : "";
   const minimalist = process.env.MINIMALIST == "true" ? true : false;
 
   return {
