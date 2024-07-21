2eb76000e75bbb2faaa3767ececcb1d938aec659
LIU Heyang <lhypds@gmail.com>
2024-07-22 00:58:29 +0900

    Small fix

--- components/Settings.js
+++ components/Settings.js
@@ -94,12 +94,14 @@ function Settings() {
         {message && <div>
           {<div className="ml-2">{message}</div>}
         </div>}
-        <div className="mt-3">- {t("Language")}</div>
-        <div className="flex flex-wrap items-center mt-2">
-          {languages && languages.map((lang) => (
-            <button className="ml-2 mb-1" key={lang.language_code} onClick={handleSettingUpdate("lang", lang.language_code + " force")}>{lang.native_name}</button>
-          ))}
-        </div>
+        {languages && <div>
+          <div className="mt-3">- {t("Language")}</div>
+          <div className="flex flex-wrap items-center mt-2">
+            {languages.map((lang) => (
+              <button className="ml-2 mb-1" key={lang.language_code} onClick={handleSettingUpdate("lang", lang.language_code + " force")}>{lang.native_name}</button>
+            ))}
+          </div>
+        </div>}
         <div className="mt-2">- {t("Email Subscription")}</div>
         <div className="flex flex-wrap items-center mt-2">
           <button className="ml-2" onClick={handleSubscribe("1")}>{t("Subscribe")}</button>
