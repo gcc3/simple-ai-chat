c84035e5ee251ec51050fcb94e6d227d1f498ed4
LIU Heyang <lhypds@gmail.com>
2024-07-27 13:41:32 +0900

    Add timestamp in pm2 log

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
     script: 'npm',
     args: 'start',
     interpreter: '/root/.nvm/versions/node/v21.5.0/bin/node',
+    time: true,  // show timestamp in logs
     // watch: true,   // restart on file changes
     env: {
       PORT: 3000,
