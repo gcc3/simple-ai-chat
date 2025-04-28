ca9332e646142a58a8cf9c4fcd834be6fe30385e
Heyang Liu <lhypds@gmail.com>
2025-04-28 21:50:04 +0900

    Rename translator

---
+++ .script/translator/README.md
@@ -0,0 +1,19 @@
+
+Application Translator
+======================
+
+
+Modules
+-------
+
+1. scan.py
+
+Scan the target folder all files, and collect the translate key into `keys.csv`.
+
+2. translate.py  
+
+Translate keys in `keys.csv` to the target language.
+
+3. delete.py  
+
+Delete the JSON key, in target language files.  
--- .script/translator/translate.py
+++ .script/translator/translate.py
@@ -62,20 +62,20 @@ with open("languages.csv", newline="", encoding="utf-8") as csvfile:
         code, name = row
         languages[code] = name
 
-# Read the target keys and associated file names from target_keys.csv
+# Read the target keys and associated file names from keys.csv
 # and map them to each file
-target_keys_per_file = {}
-with open("target_keys.csv", newline="", encoding="utf-8") as csvfile:
+keys_per_file = {}
+with open("keys.csv", newline="", encoding="utf-8") as csvfile:
     csv_reader = csv.reader(csvfile)
     for row in csv_reader:
         file_name, key = row
-        if file_name not in target_keys_per_file:
-            target_keys_per_file[file_name] = set()
-        target_keys_per_file[file_name].add(key)
+        if file_name not in keys_per_file:
+            keys_per_file[file_name] = set()
+        keys_per_file[file_name].add(key)
 
 # Read the source language file to get the base values for translation
 source_data = {}
-for file_name in target_keys_per_file:
+for file_name in keys_per_file:
     source_file_path = os.path.join(
         base_path, source_language_code, file_name + ".json"
     )
@@ -88,7 +88,7 @@ for lang_code, lang_name in languages.items():
     if lang_code == source_language_code:
         continue  # Skip the source language
 
-    for file_name, keys_to_translate in target_keys_per_file.items():
+    for file_name, keys_to_translate in keys_per_file.items():
         target_file_path = os.path.join(base_path, lang_code, file_name + ".json")
 
         # If the target file exists, read it, otherwise create a new dictionary
