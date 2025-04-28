34bab9fdd21d04427ce5fdfce3d2f8342c956dc6
Heyang Liu <lhypds@gmail.com>
2025-04-29 00:05:33 +0900

    Update packages

--- components/Subscription.js
+++ components/Subscription.js
@@ -146,11 +146,11 @@ function Subscription() {
             {((user.role_expires_at !== null && getRoleLevel(user.role) > getRoleLevel(targetRole) && user.role_expires_at > new Date()) 
            || (user.role_expires_at === null && getRoleLevel(user.role) > getRoleLevel(targetRole)))
             && <div>
-              - { t("You are a `{user.role}`, you can downgrade to `{targetRole}` after your current subscription expires.", { role: user.role, targetRole: targetRole }) }
+              - { t("You are a `{{role}}`, you can downgrade to `{{targetRole}}` after your current subscription expires.", { role: user.role, targetRole: targetRole }) }
               </div>}
             {<div className="mt-1">
               {user.role_expires_at === null && getRoleLevel(user.role) >= getRoleLevel(targetRole) && <div>
-                  - { t("You already have an unlimited expiration date for `{user.role}`.", { role: user.role }) }
+                  - { t("You already have an unlimited expiration date for `{{role}}`.", { role: user.role }) }
                 </div>}
               {((getRoleLevel(targetRole) > getRoleLevel(user.role))
                || (targetRole === user.role && user.role_expires_at !== null)
