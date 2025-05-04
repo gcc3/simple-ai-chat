29d26b1fec1e17b8fba3459e28d3d9a05a48fb12
Heyang Liu <lhypds@gmail.com>
2025-05-04 21:08:25 +0900

    Add local model engine support

--- cli.js
+++ cli.js
@@ -10,6 +10,8 @@ import { initializeSessionMemory } from "./utils/sessionUtils.js";
 import { pingOllamaAPI, listOllamaModels } from "./utils/ollamaUtils.js";
 import { loadConfig } from "./utils/configUtils.js";
 import { setTime } from "./utils/sessionUtils.js";
+import { Readable } from "stream";
+import { OpenAI } from "openai";
 
 
 // Simulate a localStorage and sessionStorage in Node.js
@@ -20,7 +22,7 @@ globalThis.localStorage = new LocalStorage('./.scratch');
 globalThis.sessionStorage = require("node-sessionstorage");
 
 
-// Monkey-patch the fetch function to use the BASE_URL and handle cookies
+// Monkey-patch the fetch function to use the server's base URL and handle cookies
 globalThis.serverBaseUrl = "https://simple-ai.io";
 const cookieJar = new tough.CookieJar();  // Handle cookies
 const fetch_ = globalThis.fetch;  // Save the original fetch function
@@ -36,6 +38,7 @@ globalThis.fetch = async (url, options) => {
 console.log = function() {};
 
 
+// M1. Generate SSE
 async function generate_sse(input, images=[], files=[]) {
   // Config (input)
   const config = loadConfig();
@@ -62,7 +65,7 @@ async function generate_sse(input, images=[], files=[]) {
     use_system_role: "true",
   });
 
-  const url = `${BASE_URL}/api/generate_sse?${params.toString()}`;
+  const url = `${globalThis.serverBaseUrl}/api/generate_sse?${params.toString()}`;
   const res = await fetch(url);
   if (!res.ok) {
     throw new Error(`[${res.status}] ${await res.text()}`);
@@ -147,7 +150,171 @@ async function generate_sse(input, images=[], files=[]) {
   }
 }
 
+// M2. Generate message from server, and then call local model engine
+async function generate_msg(input, images=[], files=[]) {
+  // Input
+  console.log("Input: " + input);
+  if (images.length > 0) console.log("Images: " + images.join(", "));
+  if (files.length > 0)  console.log("Files: " + files.join(", "));
 
+  // Output
+  let output = "";
+  
+  // Config (input)
+  const config = loadConfig();
+  console.log("Config: " + JSON.stringify(config));
+
+  // Model switch
+  const use_vision = images && images.length > 0;
+  const model = config.model;
+
+  // Generate messages
+  const msgResponse = await fetch("/api/generate_msg", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        user_input: input,
+        images: images,
+        files: files,
+        time: config.time,
+        session: config.session,
+        model: config.model,
+        mem_length: config.mem_length,
+        functions: config.functions,
+        role: config.role,
+        stores: config.stores,
+        node: config.node,
+        use_stats: config.use_stats,
+        use_eval: config.use_eval,
+        use_location: config.use_location,
+        location: config.location,
+        lang: config.lang,
+        use_system_role: config.use_system_role,
+    }),
+  });
+
+  const msgData = await msgResponse.json();
+  if (msgResponse.status !== 200) {
+    throw msgData.error || new Error(`Request failed with status ${msgResponse.status}`);
+  }
+  const msg = msgData.result.msg;
+
+  // Use stream
+  const useStream = localStorage.getItem('useStream') === "true";
+
+  // User
+  const user = {
+    username: localStorage.getItem("user")
+  }
+
+  const openai = new OpenAI({
+    baseURL: config.base_url,
+    apiKey: "",  // not necessary for local model, but required for OpenAI API
+    dangerouslyAllowBrowser: true,
+  });
+
+  // OpenAI chat completion!
+  const chatCompletion = await openai.chat.completions.create({
+    messages: msg.messages,
+    model: model,
+    frequency_penalty: 0,
+    logit_bias: null,
+    n: 1,
+    presence_penalty: 0,
+    response_format: null,
+    seed: null,
+    service_tier: null,
+    stream: useStream,
+    stream_options: null,
+    temperature: 1,
+    top_p: 1,
+    tools: null,  // TODO
+    tool_choice: null,  // TODO
+    user: user ? user.username : null,
+  });
+
+  // Record log (chat history)
+  const logadd = async (input, output) => {
+    const response1 = await fetch("/api/log/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        input,
+        output,
+        model: model,
+        session: sessionStorage.getItem("session"),
+        images: [],
+        time: Date.now(),
+      }),
+    });
+
+    if (response1.status !== 200) {
+      throw msgData.error || new Error(`Request failed with status ${response1.status}`);
+    }
+  }
+
+  // Non-stream mode
+  if (!useStream) {
+    // Get result
+    const choices = chatCompletion.choices;
+    if (!choices || choices.length === 0 || choices[0].message === null) {
+      console.error("No choice\n");
+      printOutput("Silent...");
+      return;
+    } else {
+      // 1. handle message output
+      const content = choices[0].message.content;
+      if (content) {
+        output += choices[0].message.content;
+      }
+
+      // 2. handle tool calls
+      // Not support yet.
+    }
+
+    // Add log
+    logadd(input, output);
+  }
+
+  // Stream mode
+  if (useStream) {
+    // Convert the response stream into a readable stream
+    const stream = Readable.from(chatCompletion);
+
+    // Handle the data event to process each JSON line
+    stream.on('data', (chunk) => {
+      try {
+        // 1. handle message output
+        const content = chunk.choices[0].delta.content;
+        if (content) {
+          output += content;
+          printOutput(content, false, true);
+        }
+
+        // 2. handle tool calls
+        // Not support yet.
+      } catch (error) {
+        console.error('Error parsing JSON line:', error);
+        stream.destroy(error); // Destroy the stream on error
+      }
+    });
+
+    // Resolve the Promise when the stream ends
+    stream.on('end', async () => {
+      // Add log
+      logadd(input, output);
+    });
+
+    // Reject the Promise on error
+    stream.on('error', (error) => {
+      printOutput(error);
+    });
+  }
+}
 
 // Function to print output
 function printOutput(output, append=false) {
@@ -281,12 +448,12 @@ program
 
     // Command line start
     while (true) {
-      const line = (await ask(globalThis.model + "> ")).trim();
-      if (!line) continue;
+      const input = (await ask(globalThis.model + "> ")).trim();
+      if (!input) continue;
 
-      if (line.toLowerCase() === ":exit") break;
-      if (line.startsWith(":")) {
-        const commandResult = await command(line, []);
+      if (input.toLowerCase() === ":exit") break;
+      if (input.startsWith(":")) {
+        const commandResult = await command(input, []);
         if (commandResult) {
           printOutput(commandResult.trim() + "\n");
         }
@@ -294,8 +461,22 @@ program
       }
 
       try {
-        // Stream output
-        await generate_sse(line);
+        // Generation mode switch
+        if (globalThis.baseUrl.includes("localhost") 
+          || globalThis.baseUrl.includes("127.0.0.1")) {
+          // Local model
+          console.log("Start. (Local)");
+          await generate_msg(input);
+        } else {
+          // Server model
+          if (localStorage.getItem('useStream') == "true") {
+            console.log("Start. (SSE)");
+            await generate_sse(input);
+          } else {
+            // TODO
+            printOutput("Not support yet for non-stream mode.");
+          }
+        }
       } catch (e) {
         console.error("Error:", e.message + "\n");
       }
