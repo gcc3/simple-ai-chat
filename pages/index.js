a662a9422038795f35689480200925c82aa4cf1d
Heyang Liu <lhypds@gmail.com>
2025-05-06 02:39:18 +0900

    Found cause

--- pages/index.js
+++ pages/index.js
@@ -2511,7 +2511,7 @@ export default function Home() {
           <div id="wrapper" ref={elWrapperRef} className={styles.wrapper}>
             {outputImages.map((image, index) => (
               <div key={index} className="mb-5 image-preview">
-                <Image
+                {/* <Image
                   src={image.src}
                   alt={image.alt}
                   placeholder="blur"
@@ -2521,7 +2521,7 @@ export default function Home() {
                   quality={100}
                   style={{ width: '100%', height: '100%' }}
                   unoptimized
-                />
+                /> */}
               </div>
             ))}
             <div 
