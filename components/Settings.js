2cca1c116b1c12bdced4ca1e777a4b8983cc93a2
Heyang Liu <lhypds@gmail.com>
2025-05-10 01:24:54 +0900

    Set resouce

--- components/Settings.js
+++ components/Settings.js
@@ -3,7 +3,8 @@ import { fetchUserInfo } from "utils/userUtils";
 import { useTranslation } from "react-i18next";
 import { setRtl } from "utils/rtlUtils.js";
 import { getFunctions, getMcpFunctions } from "../function.js";
-import { getActiveStores } from "../utils/storageUtils.js";
+import { updateUserSetting } from '../utils/userUtils.js';
+import { addStoreToSessionStorage, countStoresInSessionStorage, getActiveStores, isStoreActive, removeStoreFromSessionStorage } from "../utils/storageUtils.js";
 
 
 function Settings() {
@@ -13,17 +14,28 @@ function Settings() {
   const [languages, setLanguages] = useState([]);
   const [lang, setLang] = useState(null);
 
+  // Data sources
+  // 1. roles
+  const [userRoles, setUserRoles] = useState([]);
+  const [currentRole, setCurrentRole] = useState(null);
+
+  // 2. functions
   const [systemFunctions, setSystemFunctions] = useState([]);
   const [mcpFunctions, setMcpFunctions] = useState([]);
+  const [enabledFunctions, setEnabledFunctions] = useState([]);
 
-  const [userNodes, setUserNodes] = useState([]);
-  const [groupNodes, setGroupNodes] = useState([]);
-  const [systemNodes, setSystemNodes] = useState([]);
-
+  // 3. stores
   const [userStores, setUserStores] = useState([]);
   const [groupStores, setGroupStores] = useState([]);
   const [systemStores, setSystemStores] = useState([]);
-
+  const [activeStores, setActiveStores] = useState([]);
+  
+  // 4. nodes
+  const [userNodes, setUserNodes] = useState([]);
+  const [groupNodes, setGroupNodes] = useState([]);
+  const [systemNodes, setSystemNodes] = useState([]);
+  const [activeNode, setActiveNode] = useState([]);
+  
   const [message, setMessage] = useState(null);
 
   const { t, i18n, ready } = useTranslation("settings");
@@ -49,6 +61,7 @@ function Settings() {
 
       // Load data sources settings
       await Promise.all([
+        listRoles(),
         listFunctions(),
         listStores(),
         listNodes()
@@ -57,13 +70,49 @@ function Settings() {
     loadBasicSettings();
 
     // 1. List roles
-    // Not supported yet.
+    const listRoles = async () => {
+      try {
+        const response = await fetch("/api/role/list", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        const data = await response.json();
+        if (response.status !== 200) {
+          throw data.error || new Error(`Request failed with status ${response.status}`);
+        }
+  
+        if (data.result.system_roles.length === 0 && (!data.result.user_roles || Object.entries(data.result.user_roles).length === 0)) {
+          return "No role found.";
+        } else {
+          if (localStorage.getItem("user")) {
+            if (data.result.user_roles && Object.entries(data.result.user_roles).length > 0) {
+              let roles = [];
+              Object.entries(data.result.user_roles).forEach(([key, value]) => {
+                roles.push(value.role);
+              });
+              setUserRoles(roles);
+            }
+          }
+  
+          if (sessionStorage.getItem("role")) {
+            const currentRole = sessionStorage.getItem("role");
+            setCurrentRole(currentRole);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
     // 2. List functions
     const listFunctions = async () => {
       let functions = getFunctions();
       let mcpFunctionList = await getMcpFunctions();
       const enabledFunctions = (localStorage.getItem("functions")).split(",");
+      setEnabledFunctions(enabledFunctions);
 
       // System functions
       setSystemFunctions(functions.map((f) => {
@@ -98,6 +147,7 @@ function Settings() {
         } else {
           // For adding star to current store
           const activeStores = getActiveStores();
+          setActiveStores(activeStores);
   
           // User stores
           if (data.result.user_stores && Object.entries(data.result.user_stores).length > 0) {
@@ -153,6 +203,7 @@ function Settings() {
         } else {
           // For adding star to current store
           const currentNode = sessionStorage.getItem("node");
+          setActiveNode(currentNode);
   
           // User nodes
           if (data.result.user_nodes && Object.entries(data.result.user_nodes).length > 0) {
@@ -225,6 +276,142 @@ function Settings() {
     }
   }
 
+  const handleSetUserRoles = useCallback((name) => async () => {
+    if (sessionStorage.getItem("role") === name) {
+      sessionStorage.setItem("role", "");
+    } else {
+      sessionStorage.setItem("role", name);
+    }
+
+    // Update state
+    const currentRole = sessionStorage.getItem("role");
+    setCurrentRole(currentRole);
+  }, []);
+
+  const handleSetSystemFunctions = useCallback((name) => async () => {
+    const currentFunctions = (localStorage.getItem("functions")).split(",");
+    if (currentFunctions.includes(name)) {
+      const index = currentFunctions.indexOf(name);
+      currentFunctions.splice(index, 1);
+      localStorage.setItem("functions", currentFunctions.join(","));
+
+      // Update user setting (remote)
+      if (localStorage.getItem("user")) {
+        updateUserSetting("functions", currentFunctions.join(","));
+      }
+    } else {
+      currentFunctions.push(name)
+      localStorage.setItem("functions", currentFunctions.join(","));
+
+      // Update user setting (remote)
+      if (localStorage.getItem("user")) {
+        updateUserSetting("functions", currentFunctions.join(","));
+      }
+    }
+
+    // Update state
+    const enabledFunctions = (localStorage.getItem("functions")).split(",");
+    setEnabledFunctions(enabledFunctions);
+  }, []);
+
+  const handleSetMcpFunctions = useCallback((name) => async () => {
+    const currentFunctions = (localStorage.getItem("functions")).split(",");
+    if (currentFunctions.includes(name)) {
+      const index = currentFunctions.indexOf(name);
+      currentFunctions.splice(index, 1);
+      localStorage.setItem("functions", currentFunctions.join(","));
+
+      // Update user setting (remote)
+      if (localStorage.getItem("user")) {
+        updateUserSetting("functions", currentFunctions.join(","));
+      }
+    } else {
+      currentFunctions.push(name)
+      localStorage.setItem("functions", currentFunctions.join(","));
+
+      // Update user setting (remote)
+      if (localStorage.getItem("user")) {
+        updateUserSetting("functions", currentFunctions.join(","));
+      }
+    }
+
+    // Update state
+    const enabledFunctions = (localStorage.getItem("functions")).split(",");
+    setEnabledFunctions(enabledFunctions);
+  }, []);
+
+  const handleSetUserStores = useCallback((name) => async () => {
+    if (isStoreActive(name)) {
+      removeStoreFromSessionStorage(storeName);
+    } else {
+      addStoreToSessionStorage(name);
+    }
+
+    // Update state
+    const activeStores = getActiveStores();
+    setActiveStores(activeStores);
+  }, []);
+
+  const handleSetGroupStores = useCallback((name) => async () => {
+    if (isStoreActive(name)) {
+      removeStoreFromSessionStorage(storeName);
+    } else {
+      addStoreToSessionStorage(name);
+    }
+
+    // Update state
+    const activeStores = getActiveStores();
+    setActiveStores(activeStores);
+  }, []);
+
+  const handleSetSystemStores = useCallback((name) => async () => {
+    if (isStoreActive(name)) {
+      removeStoreFromSessionStorage(storeName);
+    } else {
+      addStoreToSessionStorage(name);
+    }
+
+    // Update state
+    const activeStores = getActiveStores();
+    setActiveStores(activeStores);
+  }, []);
+
+  const handleSetUserNodes = useCallback((name) => async () => {
+    if (sessionStorage.getItem("node") === name) {
+      sessionStorage.setItem("node", "");
+    } else {
+      sessionStorage.setItem("node", name);
+    }
+
+    // Update state
+    const currentNode = sessionStorage.getItem("node");
+    setActiveNode(currentNode);
+  }, []);
+
+  const handleSetGroupNodes = useCallback((name) => async () => {
+    if (sessionStorage.getItem("node") === name) {
+      sessionStorage.setItem("node", "");
+    } else {
+      sessionStorage.setItem("node", name);
+    }
+
+    // Update state
+    const currentNode = sessionStorage.getItem("node");
+    setActiveNode(currentNode);
+  }, []);
+
+  const handleSetSystemNodes = useCallback((name) => async () => {
+    if (sessionStorage.getItem("node") === name) {
+      sessionStorage.setItem("node", "");
+    } else {
+      sessionStorage.setItem("node", name);
+    }
+
+    // Update state
+    const currentNode = sessionStorage.getItem("node");
+    setActiveNode(currentNode);
+  }, []);
+
   const handleSetLanguage = useCallback((newLang) => async () => {
     // Set language
     const lang_ = newLang.replace("force", "").trim()
@@ -270,6 +457,141 @@ function Settings() {
     <>
       {message && <div>
         {<div className="ml-2">{message}</div>}
+      </div>}
+      {userRoles && userRoles.length > 0 && <div>
+        <div className="mt-3">- {t("User Roles")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {userRoles.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetUserRoles(i)}
+              style={{ backgroundColor: currentRole === i ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {systemFunctions && systemFunctions.length > 0 && <div>
+        <div className="mt-3">- {t("System Functions")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {systemFunctions.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetSystemFunctions(i)}
+              style={{ backgroundColor: enabledFunctions.includes(i) ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {mcpFunctions && mcpFunctions.length > 0 && <div>
+        <div className="mt-3">- {t("MCP Functions")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {mcpFunctions.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetMcpFunctions(i)}
+              style={{ backgroundColor: enabledFunctions.includes(i) ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {userStores && userStores.length > 0 && <div>
+        <div className="mt-3">- {t("User Stores")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {userStores.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetUserStores(i)}
+              style={{ backgroundColor: activeStores.includes(i) ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {groupStores && groupStores.length > 0 && <div>
+        <div className="mt-3">- {t("Group Stores")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {groupStores.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetGroupStores(i)}
+              style={{ backgroundColor: activeStores.includes(i) ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {systemStores && systemStores.length > 0 && <div>
+        <div className="mt-3">- {t("System Stores")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {systemStores.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetSystemStores(i)}
+              style={{ backgroundColor: activeStores.includes(i) ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {userNodes && userNodes.length > 0 && <div>
+        <div className="mt-3">- {t("User Nodes")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {userNodes.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetUserNodes(i)}
+              style={{ backgroundColor: activeNode === i ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {groupNodes && groupNodes.length > 0 && <div>
+        <div className="mt-3">- {t("Group Nodes")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {groupNodes.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetGroupNodes(i)}
+              style={{ backgroundColor: activeNode === i ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
+      </div>}
+      {systemNodes && systemNodes.length > 0 && <div>
+        <div className="mt-3">- {t("System Nodes")}</div>
+        <div className="flex flex-wrap items-center mt-2">
+          {systemNodes.map((i) => (
+            <button 
+              className="ml-2 mb-1" 
+              key={i}
+              onClick={handleSetSystemNodes(i)}
+              style={{ backgroundColor: currentRole === i ? '#EAEAEA' : '' }}
+            >
+              {i}
+            </button>
+          ))}
+        </div>
       </div>}
       {languages && <div>
         <div className="mt-3">- {t("Language")}</div>
