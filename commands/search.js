82ce91315da7db63dc04c4ae0142a7063cd27edf
Heyang Liu <lhypds@gmail.com>
2026-03-01 16:20:35 +0900

    Move search command

--- commands/search.js
+++ commands/search.js
@@ -1,5 +1,3 @@
-import { getActiveStores } from "../utils/storageUtils.js";
-
 export default async function search(args) {
   if (args.length != 1) {
     return "Usage: :search [text]";
@@ -8,46 +6,7 @@ export default async function search(args) {
   if (!args[0].startsWith("\"") || !args[0].endsWith("\"")) {
     return "Search text must be quoted with double quotes.";
   }
-  const search = args[0].slice(1, -1);
+  const searchFor = args[0].slice(1, -1);
 
-  const activeStores = getActiveStores();
-  if (activeStores.length === 0) {
-    return "No store selected.";
-  }
-
-  // TODO, use Promise.all() to make this faster
-  let results = [];
-  for (const store of activeStores) {
-    try {
-      // This will search a single store
-      const response = await fetch("/api/store/search", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          search,
-          store,
-        }),
-      });
-  
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-  
-      if (data.success) {
-        const message = "Store: " + store + "\n"
-                       + data.message;
-        results.push(message.trim());
-      } else {
-        return data.error;
-      }
-    } catch (error) {
-      console.error(error);
-      return error;
-    }
-  }
-
-  return results.join("\n\n");
+  return "Feature under development.";
 }
