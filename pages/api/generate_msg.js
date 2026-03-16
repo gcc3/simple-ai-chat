b9e040784c88ad946c2c4f4439a282871edc2286
Heyang Liu <lhypds@gmail.com>
2026-03-16 22:03:48 +0900

    Small fix

--- pages/api/generate_msg.js
+++ pages/api/generate_msg.js
@@ -115,7 +115,7 @@ export default async function(req, res) {
     console.log("\n--- configuration info ---\n"
     + "lang: " + lang + "\n"
     + "model: " + model_ + "\n"
-    + "base_url:" + "(local)" + "\n"
+    + "base_url: " + "(local)" + "\n"
     + "temperature: " + sysconf.temperature + "\n"
     + "top_p: " + sysconf.top_p + "\n"
     + "use_system_role: " + use_system_role + "\n"
