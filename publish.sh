7a31bdadac9b1b34f515d304ce41a587222f8521
Heyang Liu <lhypds@gmail.com>
2026-06-18 17:58:41 +0900

    Add login before publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Bump the version, publish to npm, and update the global package
+npm login
 npm version patch
 npm publish
 echo "About 3 minutes later, run \`npm i simple-ai-chat -g\` to update the global package to the latest version."
