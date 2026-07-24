1fa544f6c06848d501a6dbb53754955cc15a9961
Heyang Liu <lhypds@gmail.com>
2026-07-24 15:09:46 +0900

    Remove reasoning effect setting

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -118,7 +118,6 @@ export default async function(req, res) {
   const is_tool_calls_supported_model = model.is_tool_calls_supported === "1";
   const is_vision_model = model.is_vision === "1";
   const is_audio_model = model.is_audio === "1";
-  const is_reasoning_model = model.is_reasoning === "1";
   const is_image_model = model.is_image === "1";
 
   // Function calling (tool calls), MCP tools
@@ -460,7 +459,6 @@ export default async function(req, res) {
 
       // conditional params
       ...(is_tool_calls_supported_model && tools && tools.length > 0 ? { tools: tools, tool_choice: "auto" } : {}),
-      ...(is_reasoning_model ? { reasoning_effort: (is_tool_calls_supported_model && tools && tools.length > 0) ? "none" : "high" } : {}),
       ...(user ? { user: user.username } : {})
     });
 
