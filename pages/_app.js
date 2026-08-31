10b1f960d78abaae7368f458a6120f5cf71cacb7
Heyang Liu <lhypds@gmail.com>
2026-09-01 04:06:27 +0900

    Try fix iOS auto focus

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function App({ Component, pageProps }) {
       <Head>
         <meta 
           name="viewport" 
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
       </Head>
       
